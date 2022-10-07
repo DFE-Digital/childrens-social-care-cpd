35dce5077818d7ecb5049c9c19a5a740e129e1c0
nileshgnagepatil <nileshgnagepatil@outlook.com>
2022-10-07 16:45:45 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Childrens_Social_Care_CPD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
