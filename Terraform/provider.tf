bc56674aa73dfd721f777c2c7bca8d041ba938f3
Al West <al@tsew.net>
2022-10-10 18:21:59 +0100

    Add Terraform Provider

---
+++ Terraform/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.21.1"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "s185d01-tfstate-rg"
+    storage_account_name = "s185d01tfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  tenant_id = var.tenant_id
+}
+
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+}
