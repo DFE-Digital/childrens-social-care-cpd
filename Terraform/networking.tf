6b564ebb06996a2d2ed7386ecd367345478a8a21
Al West <al@tsew.net>
2022-10-28 11:26:26 +0100

    Comment out whitelist nsg

--- Terraform/networking.tf
+++ Terraform/networking.tf
@@ -54,7 +54,7 @@ resource "azurerm_subnet_network_security_group_association" "blockall" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-resource "azurerm_subnet_network_security_group_association" "whitelist" {
-  subnet_id                 = azurerm_subnet.frontend.id
-  network_security_group_id = azurerm_network_security_group.whitelist-nsg.id
-}
+# resource "azurerm_subnet_network_security_group_association" "whitelist" {
+#   subnet_id                 = azurerm_subnet.frontend.id
+#   network_security_group_id = azurerm_network_security_group.whitelist-nsg.id
+# }
