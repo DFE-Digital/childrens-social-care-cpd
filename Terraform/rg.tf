3889509c7a9af7ff45ef10b50c517f7cb2ab1338
Al West <al@tsew.net>
2022-10-28 11:52:42 +0100

    Use temporary tags for service

--- Terraform/rg.tf
+++ Terraform/rg.tf
@@ -6,9 +6,12 @@ resource "azurerm_resource_group" "rg" {
     "Environment"      = "Dev",
     "Parent Business"  = "Children’s Care",
     "Service Offering" = "Social Workforce",
-    "Portfolio"        = "Vulnerable Children and Families",
-    "Service Line"     = "Children and Social care",
-    "Service"          = "Children and Social care",
-    "Product"          = "Social Workforce"
+    "Portfolio"        = "Newly Onboarded",
+    "Service Line"     = "Newly Onboarded",
+    "Service"          = "Newly Onboarded"
+    # "Portfolio"        = "Vulnerable Children and Families",
+    # "Service Line"     = "Children and Social care",
+    # "Service"          = "Children and Social care",
+    "Product" = "Social Workforce"
   }
 }
