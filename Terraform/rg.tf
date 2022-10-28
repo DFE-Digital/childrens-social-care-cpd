9f38578e882889fdb30b28ce2443f3ed08913fb4
Al West <al@tsew.net>
2022-10-28 12:35:24 +0100

    Fix formatting

--- Terraform/rg.tf
+++ Terraform/rg.tf
@@ -3,14 +3,15 @@ resource "azurerm_resource_group" "rg" {
   location = "westeurope"
 
   tags = {
-    "Environment"      = "Dev",
-    "Parent Business"  = "Children’s Care",
-    "Service Offering" = "Social Workforce",
-    "Portfolio"        = "Newly Onboarded",
-    "Service Line"     = "Newly Onboarded",
+    "Environment"      = "Dev"
+    "Parent Business"  = "Children’s Care"
+    "Service Offering" = "Social Workforce"
+    "Portfolio"        = "Newly Onboarded"
+    "Service Line"     = "Newly Onboarded"
     "Service"          = "Newly Onboarded"
-    # "Portfolio"        = "Vulnerable Children and Families",
-    # "Service Line"     = "Children and Social care",
-    # "Service"          = "Children and Social care",
+    # "Portfolio"        = "Vulnerable Children and Families"
+    # "Service Line"     = "Children and Social care"
+    # "Service"          = "Children and Social care"
     "Product" = "Social Workforce"
   }
+}
