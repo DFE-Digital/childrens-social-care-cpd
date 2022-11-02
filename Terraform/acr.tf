e22dd3a433f551efd6c16dd19e9fb2c36ffcf1d8
Al West <al@tsew.net>
2022-11-02 11:59:50 +0000

    Remove ACR as using a global repository

--- Terraform/acr.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_container_registry" "acr" {
-  name                = "s185dcontainerregistry"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = false
-
-  tags = azurerm_resource_group.rg.tags
-}
