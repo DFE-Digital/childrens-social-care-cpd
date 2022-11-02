b399e5be32969372360ab4432bfc045bd375825c
Al West <al@tsew.net>
2022-11-02 13:13:05 +0000

    Add back depends_on

--- Terraform/appgw.tf
+++ Terraform/appgw.tf
@@ -69,8 +69,8 @@ resource "azurerm_application_gateway" "appgw" {
 
   tags = azurerm_resource_group.rg.tags
 
-  # depends_on = [
-  #   azurerm_network_security_group.nsg,
-  #   azurerm_subnet_network_security_group_association.blockall
-  # ]
+  depends_on = [
+    azurerm_network_security_group.nsg,
+    azurerm_subnet_network_security_group_association.blockall
+  ]
 }
