ed47c75c1063c5faa8ecfc9a2824c79f450561f4
Al West <al@tsew.net>
2022-11-04 10:40:25 +0000

    Add whitelist-rules to main nsg

--- Terraform/whitelist-nsg.tf
+++ Terraform/whitelist-nsg.tf
@@ -12,13 +12,6 @@ locals {
   ips = compact((split(" ", data.azurerm_key_vault_secret.ips.value)))
 }
 
-resource "azurerm_network_security_group" "whitelist-nsg" {
-  name                = "s185d01-chidrens-social-care-cpd-whitelist-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = azurerm_resource_group.rg.tags
-}
-
 resource "azurerm_network_security_rule" "whitelist-rules" {
   count                       = length(local.ips)
   name                        = "Allow-WhiteList-${count.index}"
@@ -31,5 +24,5 @@ resource "azurerm_network_security_rule" "whitelist-rules" {
   source_address_prefix       = local.ips[count.index]
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.whitelist-nsg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
 }
