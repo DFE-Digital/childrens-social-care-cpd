92b28ad7c17d823f760e8340729488ef289c189b
Al West <al@tsew.net>
2022-11-09 11:09:26 +0000

    Point to first frontend ip conf id

--- Terraform/application.tf
+++ Terraform/application.tf
@@ -66,7 +66,7 @@ resource "azurerm_private_link_service" "private-link" {
   name                                        = "s185d01-app-private-link"
   resource_group_name                         = azurerm_resource_group.rg.name
   location                                    = azurerm_resource_group.rg.location
-  load_balancer_frontend_ip_configuration_ids = azurerm_application_gateway.appgw.frontend_ip_configuration
+  load_balancer_frontend_ip_configuration_ids = [azurerm_application_gateway.appgw.frontend_ip_configuration.0.id]
 
   nat_ip_configuration {
     name      = "s185d01-nat-ip-app-private-link"
