4763a0ee8fda79d960ab095921c5cb3a9f65e39b
Al West <al@tsew.net>
2022-11-10 12:19:08 +0000

    Whitelist devs from KV

--- Terraform/nsg.tf
+++ Terraform/nsg.tf
@@ -6,20 +6,6 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_security_rule" "nsg-rule-01" {
-  name                        = "Allow-Al"
-  priority                    = 500
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "86.10.229.100"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
-
-resource "azurerm_network_security_rule" "nsg-rule-03" {
   name                        = "AllowAzureLoadBalancerInBound"
   priority                    = 3651
   direction                   = "Inbound"
