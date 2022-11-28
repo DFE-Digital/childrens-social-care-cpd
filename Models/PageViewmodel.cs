e8509320773dedbb8b19d8e1fc8459fc3a9f929d
nileshgnagepatil <nileshgnagepatil@outlook.com>
2022-11-28 12:07:12 +0000

    Pathway Details template

--- Models/PageViewmodel.cs
+++ Models/PageViewmodel.cs
@@ -6,12 +6,22 @@ namespace Childrens_Social_Care_CPD.Models
     {
         public ContentPageName PageName { get; set; }
 
-        public ContentPageType PageType { get; set; }
-
         public string PageTitle { get; set; }
+        
+        public string PageHeading { get; set; }
+
+        public string PageSubHeading { get; set; }
+
+        public ContentPageType PageType { get; set; }
 
         public List<Role> Roles { get; set; }
 
         public List<Paragraph> Paragraphs { get; set; }
+
+        public List<Link> Links { get; set; }
+
+        public List<Label> Labels { get; set; }
+
+        public List<RichText> RichTexts { get; set; }
     }
 }
---
+++ Models/RichText.cs
@@ -0,0 +1,15 @@
+﻿using Contentful.Core.Models;
+
+namespace Childrens_Social_Care_CPD.Models
+{
+    public class RichText
+    {
+        public string Heading { get; set; }
+
+        public string SubHeading { get; set; }
+
+        public Document RichTextContents { get; set; }
+
+        public int SortOrder { get; set; }
+    }
+}
