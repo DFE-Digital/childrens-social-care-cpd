5dc7085ebf5bd6a363ae919ce4de18085bb18421
nileshgnagepatil <nileshgnagepatil@outlook.com>
2022-11-28 12:19:27 +0000

    Removed commented code.

--- Models/Role.cs
+++ Models/Role.cs
@@ -2,14 +2,11 @@
 {
     public class Role
     {
-        //public string RolePageName { get; set; }
         public string RoleHeader { get; set; }
-        //public string RoleHeaderURL { get; set; }
         public string RoleContents { get; set; }
         public string RoleDescription { get; set; }
         public int SortOrder { get; set; }
         public ContentPageType PageType { get; set; }
         public RedirectPageName RedirectPageName { get; set; }
-        //public ContentPageName PageName { get; set; }
     }
 }
