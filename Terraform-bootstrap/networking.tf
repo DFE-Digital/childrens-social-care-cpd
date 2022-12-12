c60a2fb6edec795c30d9a1b77d707eccacca2ae0
Al West <al@tsew.net>
2022-12-12 14:31:45 +0000

    Terraform bootstrap code

---
+++ Terraform-bootstrap/networking.tf
@@ -0,0 +1,13 @@
+resource "azurerm_public_ip" "pip1" {
+  name                = var.pip_name[terraform.workspace]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = azurerm_resource_group.rg.tags
+}
