7c91ad97583b3af8497797cbd6baa1321c7bfa84
nileshgnagepatil <nileshgnagepatil@outlook.com>
2022-12-14 14:30:02 +0000

    Changed the default landing page

--- Childrens-Social-Care-CPD/Program.cs
+++ Childrens-Social-Care-CPD/Program.cs
@@ -45,6 +45,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=CPD}/{action=LandingPage}");
 
 app.Run();
