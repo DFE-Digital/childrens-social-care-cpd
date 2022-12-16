4fb5ab595ddbbcdf4dbb332e5079178cfa791767
nileshgnagepatil <nileshgnagepatil@outlook.com>
2022-12-16 14:56:54 +0000

    ViewAllProgrammes page changes

--- Childrens-Social-Care-CPD/Models/Link.cs
+++ Childrens-Social-Care-CPD/Models/Link.cs
@@ -5,6 +5,9 @@
         public string LinkText { get; set; }
         public string LinkURL { get; set; }
         public string LinkSection { get; set; }
+        public ContentPageType PageType { get; set; }
+        public ContentPageName RedirectPageName { get; set; }
+
         public int SortOrder { get; set; }
     }
 }
