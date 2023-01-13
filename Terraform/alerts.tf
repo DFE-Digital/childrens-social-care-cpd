4866c7c529acade329398c3b3076e7c22a6ac271
Al West <al@tsew.net>
2023-01-13 11:09:26 +0000

    Revert alerts

--- Terraform/alerts.tf
+++ Terraform/alerts.tf
@@ -83,6 +83,7 @@ resource "azurerm_monitor_metric_alert" "container-cpu" {
   tags = data.azurerm_resource_group.rg.tags
 }
 
+
 resource "azurerm_monitor_metric_alert" "container-avg-resp-time" {
   name                = var.alert_container_avg_resp_time[terraform.workspace]
   resource_group_name = data.azurerm_resource_group.rg.name
@@ -101,49 +102,6 @@ resource "azurerm_monitor_metric_alert" "container-avg-resp-time" {
 
   action {
     action_group_id = azurerm_monitor_action_group.main.id
-  }
-
-  tags = data.azurerm_resource_group.rg.tags
-}
-
-resource "azurerm_monitor_scheduled_query_rules_alert_v2" "exception" {
-  name                = "exception-alert"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-
-  evaluation_frequency = "PT1M"
-  window_duration      = "PT1M"
-  scopes               = [azurerm_log_analytics_workspace.log-analytics-ws.id]
-  severity             = 4
-
-  criteria {
-    query                   = <<-QUERY
-        Perf
-        | where TimeGenerated > ago(1h)
-        | where CounterName == "% Processor Time" and InstanceName == "_Total" 
-        | project TimeGenerated, Computer, CounterValue, _ResourceId
-        | summarize AggregatedValue = avg(CounterValue)  by bin(TimeGenerated, 1h), Computer, _ResourceId   
-      QUERY
-    time_aggregation_method = "Count"
-    threshold               = 1
-    operator                = "GreaterThanOrEqual"
-
-    failing_periods {
-      minimum_failing_periods_to_trigger_alert = 1
-      number_of_evaluation_periods             = 1
-    }
-  }
-
-  auto_mitigation_enabled          = false
-  workspace_alerts_storage_enabled = false
-  description                      = "This is V2 custom log alert"
-  display_name                     = "exception-alertv2"
-  enabled                          = true
-  query_time_range_override        = "P2D"
-  skip_query_validation            = false
-
-  action {
-    action_groups = [azurerm_monitor_action_group.main.id]
   }
 
   tags = data.azurerm_resource_group.rg.tags
