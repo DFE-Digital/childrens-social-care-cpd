f81b5b4a368b3256bd8e75553ba95d7ab0bf3dce
nileshgnagepatil <nileshgnagepatil@outlook.com>
2023-01-17 09:22:50 +0000

    Model for Cookie banner

---
+++ Childrens-Social-Care-CPD/Models/CookieBanner.cs
@@ -0,0 +1,14 @@
+﻿namespace Childrens_Social_Care_CPD.Models
+{
+    public class CookieBanner
+    {
+        public string BannerHeader { get; set; }
+        public List<string> BannerDescription { get; set; }
+        public string AcceptButtonText { get; set; }
+        public string RejectButtonText { get; set; }
+        public string ViewCookiesLinkText { get; set; }
+        public string HideCookieMessageButtonText { get; set; }
+        public string AcceptedCookieMessage { get; set; }
+        public string RejectedCookieMessage { get; set; }
+    }
+}
