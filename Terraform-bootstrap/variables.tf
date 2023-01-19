cecb88e394866c0d4fb5aeb24d7eb3f159d1169e
Al West <al@tsew.net>
2023-01-19 21:31:30 +0000

    Remove select resources from main Terraform code and move to Terraform-bootstrap
    * add exception in tflint for terraform_unused_declarations
    * Move vnet
    * Move appsinsights
    * Move log_analytics
    * Update README for variables

--- Terraform-bootstrap/variables.tf
+++ Terraform-bootstrap/variables.tf
@@ -22,6 +22,54 @@ variable "pip_name" {
   description = "Name of Public IP address"
 }
 
+variable "vnet_name" {
+  type = map(string)
+  default = {
+    Dev       = "s185d01-chidrens-social-care-cpd-vn01"
+    Test      = "s185d02-chidrens-social-care-cpd-vn01"
+    Load-Test = "s185d03-chidrens-social-care-cpd-vn01"
+    Pre-Prod  = "s185t01-chidrens-social-care-cpd-vn01"
+    Prod      = "s185p01-chidrens-social-care-cpd-vn01"
+  }
+  description = "Name of Address Space"
+}
+
+variable "vnet_address_space" {
+  type = map(string)
+  default = {
+    Dev       = "10.0.0.0/16"
+    Test      = "10.1.0.0/16"
+    Load-Test = "10.2.0.0/16"
+    Pre-Prod  = "10.0.0.0/16"
+    Prod      = "10.0.0.0/16"
+  }
+  description = "Subnets used for Address Space"
+}
+
+variable "appinsights_name" {
+  type = map(string)
+  default = {
+    Dev       = "s185d01-chidrens-social-care-cpd-ai"
+    Test      = "s185d02-chidrens-social-care-cpd-ai"
+    Load-Test = "s185d03-chidrens-social-care-cpd-ai"
+    Pre-Prod  = "s185t01-chidrens-social-care-cpd-ai"
+    Prod      = "s185p01-chidrens-social-care-cpd-ai"
+  }
+  description = "Name of Application Insights"
+}
+
+variable "log_analytics_ws_name" {
+  type = map(string)
+  default = {
+    Dev       = "s185d01-chidrens-social-care-cpd-la"
+    Test      = "s185d02-chidrens-social-care-cpd-la"
+    Load-Test = "s185d03-chidrens-social-care-cpd-la"
+    Pre-Prod  = "s185t01-chidrens-social-care-cpd-la"
+    Prod      = "s185p01-chidrens-social-care-cpd-la"
+  }
+  description = "Name of Log Analytics Workspace"
+}
+
 variable "key_vault_rg" {
   type = map(string)
   default = {
