61dc46e5e9fabde9499f58bd9e7b653dc2dff7bb
nileshgnagepatil <nileshgnagepatil@outlook.com>
2023-01-20 11:23:51 +0000

    Set Google Analytics cookie consent

---
+++ Childrens-Social-Care-CPD/CookieHelper.cs
@@ -0,0 +1,22 @@
+﻿using Childrens_Social_Care_CPD.Constants;
+
+namespace Childrens_Social_Care_CPD
+{
+    public static class CookieHelper
+    {
+        public static void SetAnalyticsCookie(string analyticsCookieConsent, HttpContext httpContext)
+        {
+            var options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(28),
+                HttpOnly = true,
+                SameSite = SameSiteMode.Strict,
+                IsEssential = true,
+                Secure = true
+            };
+
+            httpContext.Response.Cookies.Append(SiteConstants.ANALYTICSCOOKIENAME, analyticsCookieConsent,
+                options);
+        }
+    }
+}
