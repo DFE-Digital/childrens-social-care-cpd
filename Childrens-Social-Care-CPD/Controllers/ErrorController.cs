46531a36f2ff1ecfeb6662d34513031bd5147148
nileshgnagepatil <nileshgnagepatil@outlook.com>
2023-04-05 12:48:04 +0100

    Code refactoring

--- Childrens-Social-Care-CPD/Controllers/ErrorController.cs
+++ Childrens-Social-Care-CPD/Controllers/ErrorController.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Diagnostics;
 using System.Net;
+using Childrens_Social_Care_CPD.ActionFilters;
 
 namespace Childrens_Social_Care_CPD.Controllers
 {
+    [ServiceFilter(typeof(CPDActionFilter))]
     public class ErrorController : Controller
     {
-
         public ErrorController() 
         {
         }
