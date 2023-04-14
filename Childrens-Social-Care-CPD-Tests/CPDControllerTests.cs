7216b1b74d14689d147ae4eb35d30b67072900af
nileshgnagepatil <nileshgnagepatil@outlook.com>
2023-04-14 13:35:12 +0100

    Added common class for test mock data

--- Childrens-Social-Care-CPD-Tests/CPDControllerTests.cs
+++ Childrens-Social-Care-CPD-Tests/CPDControllerTests.cs
@@ -15,22 +15,15 @@ namespace Childrens_Social_Care_CPD_Tests
     {
         private Mock<IContentfulDataService> _contentfulDataService;
         private Mock<ILogger<CPDController>> _logger;
-        private ContentfulCollection<PageViewModel> _pages;
-        private PageFooter _footer;
-        private PageHeader _header;
-        private ContentfulCollection<CookieBanner> _banner;
         private CPDController _target;
 
         [SetUp]
         public void Setup()
         {
-            SetupModels();
-
-
             _contentfulDataService = new Mock<IContentfulDataService>(MockBehavior.Strict);
-            _contentfulDataService.Setup(c => c.GetViewData<PageViewModel>(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(_pages);
-            _contentfulDataService.Setup(c => c.GetFooterData()).ReturnsAsync(_footer);
-            _contentfulDataService.Setup(c => c.GetHeaderData()).ReturnsAsync(_header);
+            _contentfulDataService.Setup(c => c.GetViewData<PageViewModel>(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(MockData.Pages);
+            _contentfulDataService.Setup(c => c.GetFooterData()).ReturnsAsync(MockData.Footer);
+            _contentfulDataService.Setup(c => c.GetHeaderData()).ReturnsAsync(MockData.Header);
             _logger = new Mock<ILogger<CPDController>>();
             _target = new CPDController(_logger.Object, _contentfulDataService.Object);
         }
@@ -41,110 +34,6 @@ namespace Childrens_Social_Care_CPD_Tests
             var actual = _target.LandingPage(null, null, null, null);
             ViewResult viewResult = (ViewResult)actual.Result;
             Assert.IsInstanceOf<ContentfulCollection<PageViewModel>>(viewResult.Model);
-        }
-
-        private void SetupModels()
-        {
-            _pages = new ContentfulCollection<PageViewModel>
-            {
-                Items = new List<PageViewModel>
-                {
-                    new PageViewModel
-                    {
-                        PageName = new ContentPageName { PageName = "TestPage" },
-                        PageTitle = "Test Title",
-                        PageHeading = "TestHeading",
-                        PageSubHeading = "TestSubHeading",
-                        PageType = new ContentPageType { PageType = "Master" },
-                        Cards = new List<Card>
-                        {
-                            new Card
-                            {
-                                CardHeader = "TestCardHeading",
-                                CardContents = "Test contents",
-                                CardDescription = "Test Description",
-                                SortOrder = 0,
-                                PageType = null,
-                                RedirectPageName = null
-                            }
-                        },
-                        Links = new List<Link>
-                        {
-                            new Link
-                            {
-                                LinkText = "TestLink",
-                                LinkURL = "#",
-                                SortOrder = 0
-                            }
-                        },
-                        Labels = new List<Label>
-                        {
-                            new Label
-                            {
-                                labelHeading = "TestLabelHeading",
-                                LabelText = "TestLabelText",
-                                LabelType = null,
-                                LabelTextLink = "#",
-                                SortOrder = 0
-                            }
-                        },
-                        RichTexts = new List<RichText>
-                        {
-                            new RichText
-                            {
-                                Heading = "TestHeading",
-                                SubHeading = "TestSubHeading",
-                                RichTextContents = new Document(),
-                                SortOrder = 0
-                            }
-                        }
-                    }
-                }
-            };
-
-            _header = new PageHeader
-            {
-                Header = "TestPageHeader",
-                PrototypeHeader = "TestHeader",
-                PrototypeText = new Document(),
-                PrototypeTextHtml = "TestHtml",
-            };
-
-            _footer = new PageFooter()
-            {
-                        FooterLinks = new()
-                        {
-                            new Link
-                            {
-                                LinkText = "TestLink",
-                                LinkURL = "TestUrl",
-                                LinkSection = null,
-                                PageType = null,
-                                RedirectPageName = null,
-                                SideNaveGroupText = null,
-                                SortOrder = 1
-                            }
-                        },
-                        LicenceDescription = new Document(),
-                        CopyrightLink = new Link(),
-                        LicenceDescriptionText = "TestDescription"
-            };
-
-            _banner = new ContentfulCollection<CookieBanner>()
-            {
-                Items = new List<CookieBanner>()
-                {
-                   new CookieBanner ()
-                   {
-                    AcceptCookieButtonText = "AcceptAnalytics",
-                    AcceptedCookieMessage = new Document(),
-                    CookieBannerHeading = "TestCookie",
-                    ViewCookiesLink = new Link(),
-                    RejectedCookieMessage = new Document(),
-                    HideCookieMessageButtonText = "HideMessage"
-                   }
-                }
-            };
         }
     }
 }
