875da00595fe0c3ec44dfd69b37362f6279cc095
VReddy <vipin.reddy@digital.education.gov.uk>
2023-05-05 13:40:31 +0100

    added the breadcrumblinks tests

--- Childrens-Social-Care-CPD-Tests/Cypress/e2e/breadCrumblinks.js
+++ Childrens-Social-Care-CPD-Tests/Cypress/e2e/breadCrumblinks.js
@@ -1,1 +1,22 @@
-﻿
+﻿import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+
+
+Given("a user has clicked on {string}", (id) => {
+    cy.visitcpdhomepage();
+    cy.get('[datatestid="' + id + '"]').click();
+});
+
+
+Then("the user clicks on {string} link should redirect to {string}", (id, link) => {
+    cy.get('[datatestid="' + id + '"]').should('exist');
+    cy.get('[datatestid="' + id + '"]').click();
+    cy.url().should('include', link)
+});
+
+When("user clicks on {string}", (id) => {
+    cy.get('[datatestid="' + id + '"]').click();
+});
+
+When("user clicks on sub heading", () => {
+    cy.get("h3").click();
+});
