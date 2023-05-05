6b483c4291fd79deecd7b0a7a465faf8a48375ca
VReddy <vipin.reddy@digital.education.gov.uk>
2023-05-05 13:01:43 +0100

    added the cypress test for external links and footer

---
+++ Childrens-Social-Care-CPD-Tests/Cypress/e2e/footerLinks.js
@@ -0,0 +1,13 @@
+﻿import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("a user has arrived on the home page", () => {
+    cy.visitcpdhomepage();
+});
+
+Then("the {string} has link to {string}", (id, link) => {
+    cy.get('[datatestid="' + id + '"]').should(
+        "have.attr",
+        "href",
+        link
+    );
+});
