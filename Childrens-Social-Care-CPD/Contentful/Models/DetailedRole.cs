89af3ac6ee266b0374d77a1490a63ed91a14a108
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-07-13 14:44:12 +0100

    Add our POCO models and the type resolver for Contentful

---
+++ Childrens-Social-Care-CPD/Contentful/Models/DetailedRole.cs
@@ -0,0 +1,16 @@
+﻿using Contentful.Core.Models;
+
+namespace Childrens_Social_Care_CPD.Contentful.Models;
+
+public class DetailedRole : IContent
+{
+    public string Title { get; set; }
+    public string SalaryRange { get; set; }
+    public string Summary { get; set; }
+    public string OtherNames { get; set; }
+    public Document WhatYoullDo { get; set; }
+    public Document SkillsAndKnowledge { get; set; }
+    public Document HowToBecomeOne { get; set; }
+    public Document CareerPathsAndProgression { get; set; }
+    public Document CurrentOpportunities { get; set; }
+}
