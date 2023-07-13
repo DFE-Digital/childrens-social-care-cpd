abfa913fc6c7a58732e1281734bf759cb58256a3
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-07-13 13:53:18 +0100

    Add empty ContentController

---
+++ Childrens-Social-Care-CPD/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using Childrens_Social_Care_CPD.Interfaces;
+using Contentful.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Childrens_Social_Care_CPD.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly IContentfulClient _client;
+        private readonly IContentfulDataService _dataService;
+
+        public ContentController(IContentfulClient client, IContentfulDataService dataService)
+        {
+            _client = client;
+            _dataService = dataService;
+        }
+
+        [Route("content/")]
+        [Route("content/{*pagename:regex(^[[0-9a-z]](\\/?[[0-9a-z\\-]])*\\/?$)}")] // filter permissable page name format
+        public IActionResult Index(string pageName)
+        {
+            return View();
+        }
+    }
+}
