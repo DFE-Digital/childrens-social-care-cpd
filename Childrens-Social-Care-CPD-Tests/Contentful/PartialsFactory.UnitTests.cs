5a84f77c208cb9e5573d3d59c726422365f15edf
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-07-13 15:18:39 +0100

    Add PartialsFactory

---
+++ Childrens-Social-Care-CPD-Tests/Contentful/PartialsFactory.UnitTests.cs
@@ -0,0 +1,54 @@
+﻿using Childrens_Social_Care_CPD.Contentful;
+using Childrens_Social_Care_CPD.Contentful.Models;
+using Contentful.Core.Models;
+using NUnit.Framework;
+
+namespace Childrens_Social_Care_CPD_Tests.Contentful
+{
+    [TestFixture]
+    public class PartialsFactoryTests
+    {
+        public static object[] Successful_Resolves =
+        {
+            new object[] { new ColumnLayout(), "./ColumnLayout" },
+            new object[] { new Content(), "./Content" },
+            new object[] { new ContentLink(), "./ContentLink" },
+            new object[] { new ContentSeparator(), "./ContentSeparator" },
+            new object[] { new DetailedRole(), "./DetailedRole" },
+            new object[] { new HeroBanner(), string.Empty },
+            new object[] { new LinkCard(), "./LinkCard" },
+            new object[] { new LinkListCard(), "./LinkListCard" },
+            new object[] { new RichTextBlock(), "./RichTextBlock" },
+            new object[] { new RoleList(), "./RoleList" },
+            new object[] { new SideMenu(), "./SideMenu" },
+            new object[] { new TextBlock(), "./TextBlock" },
+        };
+
+        [TestCaseSource(nameof(Successful_Resolves))]
+        public void Resolves_Correctly(IContent item, string expectedPartialName)
+        {
+            var actual = PartialsFactory.GetPartialFor(item);
+            
+            Assert.AreEqual(expectedPartialName, actual);
+        }
+
+        public class RandomTestingType : IContent { }
+
+        public static IContent[] Unsuccessful_Resolves =
+        {
+            new RandomTestingType(),
+            null
+        };
+
+        [TestCaseSource(nameof(Unsuccessful_Resolves))]
+        public void Does_Not_Resolve_Unknown_Content(IContent item)
+        {
+
+            var expectedPartialName = "./UnknownContentWarning";
+
+            var actual = PartialsFactory.GetPartialFor(item);
+
+            Assert.AreEqual(expectedPartialName, actual);
+        }
+    }
+}
---
+++ Childrens-Social-Care-CPD/Contentful/PartialsFactory.cs
@@ -0,0 +1,32 @@
+﻿using Childrens_Social_Care_CPD.Contentful.Models;
+using Contentful.Core.Models;
+
+namespace Childrens_Social_Care_CPD.Contentful;
+
+public static class PartialsFactory
+{
+    /// <summary>
+    /// Maps the POCO models to their respective Partial Views.
+    /// </summary>
+    /// <param name="item">An IContent instance</param>
+    /// <returns>The partial view used to render that object.</returns>
+    public static string GetPartialFor(IContent item)
+    {
+        switch (item)
+        {
+            case (ColumnLayout): return "./ColumnLayout";
+            case (Content): return "./Content";
+            case (ContentLink): return "./ContentLink";
+            case (ContentSeparator): return "./ContentSeparator";
+            case (DetailedRole): return "./DetailedRole";
+            case (HeroBanner): return string.Empty; // skip - handled in pre-render section
+            case (LinkCard): return "./LinkCard";
+            case (LinkListCard): return "./LinkListCard";
+            case (RichTextBlock): return "./RichTextBlock";
+            case (RoleList): return "./RoleList";
+            case (SideMenu): return "./SideMenu";
+            case (TextBlock): return "./TextBlock";
+            default : return "./UnknownContentWarning";
+        }
+    }
+}
