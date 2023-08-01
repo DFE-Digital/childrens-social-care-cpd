ba52c8bf4983b50f81bfeccfb39853df0e1faf19
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-01 16:16:23 +0100

    Fix the double header/footer issue and make the ErrorController render specific views for each error

---
+++ Childrens-Social-Care-CPD-Tests/Controllers/ErrorController.ServerTests.cs
@@ -0,0 +1,62 @@
+﻿using Childrens_Social_Care_CPD.Contentful.Models;
+using Contentful.Core.Models;
+using Contentful.Core.Search;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Childrens_Social_Care_CPD_Tests.Controllers;
+
+public class ErrorControllerServerTests
+{
+    private CpdTestServerApplication _application;
+    private HttpClient _httpClient;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _application = new CpdTestServerApplication();
+        _httpClient = _application.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = true
+        });
+    }
+
+    [Test]
+    public async Task Non_Existant_Page_Will_Return_404()
+    {
+        // arrange
+        var contentCollection = new ContentfulCollection<Content>() { Items = new List<Content>() };
+        _application.CpdContentfulClient.GetEntries(Arg.Any<QueryBuilder<Content>>(), default).Returns(contentCollection);
+        var url = "/does_not_exist";
+
+        // act
+        var response = await _httpClient.GetAsync(url);
+        
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        response.RequestMessage.RequestUri.AbsolutePath.Should().Be(url);
+    }
+
+    [Test]
+    public async Task Exception_Will_Return_500()
+    {
+        // arrange
+        _application.CpdContentfulClient.GetEntries(Arg.Any<QueryBuilder<Content>>(), default).Throws(new Exception("Test exception"));
+        var url = "/content/something";
+
+        // act
+        var response = await _httpClient.GetAsync(url);
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        response.RequestMessage.RequestUri.AbsolutePath.Should().Be(url);
+    }
+}
