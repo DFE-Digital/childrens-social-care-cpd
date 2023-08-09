fd0cf87e5a8835a2b3b4475ca88845a604129ff2
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-09 14:11:19 +0100

    Test for error page header matching the main site

--- browser-tests/dev-integration-tests/tests/errors.spec.ts
+++ browser-tests/dev-integration-tests/tests/errors.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Error conditions', () => {
 
+
+    
     test('When a non existant page is visited, the 404 page is shown', async ({ page }) => {
         await page.goto('does-not-exist')
 
@@ -12,6 +14,43 @@ test.describe('Error conditions', () => {
         await page.goto('does-not-exist')
 
         await expect(page).toHaveURL(/\/does-not-exist/)
+    })
+
+    test('Error page header nav is synced with main site header', async ({ page }) => {
+
+        // get everything we need from the home page first
+        await page.goto('content')
+        
+        const mainNavs = await page.locator("[id^=mmi-]")
+        const mainNavCount = await mainNavs.count();
+        const mainPageNavs = new Map()
+
+        for (let index = 0; index < mainNavCount; index++)
+        {
+            const nav = await mainNavs.nth(index)
+            const id = await nav.getAttribute("id")
+            const anchor = nav.locator("a")
+            const text = await anchor.innerText()
+            const href = await anchor.getAttribute("href")
+            
+            mainPageNavs.set(id, { text, href })
+        }
+
+        // now we can compare to the error page
+        await page.goto('does-not-exist')
+        const errorNavs = await page.locator("[id^=mmi-]")
+        const errorNavCount = await errorNavs.count()
+
+        expect(mainNavCount).toEqual(errorNavCount)
+
+        for (const [key, value] of mainPageNavs)
+        {
+            const anchor = await page.locator(`[id=${key}] a`)
+
+            await expect(anchor).toBeAttached();
+            await expect(anchor).toHaveAttribute('href', value.href)
+            await expect(anchor).toHaveText(value.text)
+        }
     })
 
 })
