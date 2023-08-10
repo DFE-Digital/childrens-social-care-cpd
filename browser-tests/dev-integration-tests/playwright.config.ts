bdfb29b5bf3ebe70417c7c48a372cc59b7686abf
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-10 13:59:01 +0100

    Update playwright.config.ts

--- browser-tests/dev-integration-tests/playwright.config.ts
+++ browser-tests/dev-integration-tests/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'line',
+  reporter: process.env.CI ? 'github' : 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,8 +30,6 @@ export default defineConfig({
     trace: 'on-first-retry',
 
     screenshot: 'only-on-failure',
-
-    //actionTimeout: 5000,
   },
 
   /* Configure projects for major browsers */
@@ -72,10 +70,4 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
