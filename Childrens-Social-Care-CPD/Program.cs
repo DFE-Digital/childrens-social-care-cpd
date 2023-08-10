ef88b703c37bd8e73806113dfdc36312bb10741f
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-10 13:54:01 +0100

    Hsts in dev mode seems pointless

--- Childrens-Social-Care-CPD/Program.cs
+++ Childrens-Social-Care-CPD/Program.cs
@@ -40,13 +40,6 @@ var options = new ApplicationInsightsServiceOptions
 builder.Services.AddApplicationInsightsTelemetry(options: options);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseResponseCompression();
 app.UseExceptionHandler("/error/error");
 app.UseStatusCodePagesWithReExecute("/error/{0}");
