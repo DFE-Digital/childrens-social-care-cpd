374139af10ebb56e86bee5c9ce2f29625673e951
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-14 09:59:34 +0100

    Add the line reporter back in again.

--- browser-tests/dev-integration-tests/playwright.config.ts
+++ browser-tests/dev-integration-tests/playwright.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    ['line'],
     ['html', { open: 'never' }],
   ],
   //process.env.CI ? 'github' : 'line',
