529745a26ce33819580075627d9f68da8c08f45e
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-15 11:50:21 +0100

    Remove unused constants

--- Childrens-Social-Care-CPD/SiteConstants.cs
+++ Childrens-Social-Care-CPD/SiteConstants.cs
@@ -2,23 +2,12 @@
 
 public static class SiteConstants
 {
-    public const string PAGEHEADER = "pageHeader";
-    public const string SECTION = "section";
-    public const string PAGE = "page";
-    public const string PAGENAMES = "pageNames";
-    public const string PAGENAME = "pageName";
-    public const string PAGETYPE = "pageType";
-    public const string SENDINGPAGE = "sendingPage";
-    public const string SENDINGPAGETYPE = "sendingPageType";
-    public const string COOKIEBANNER = "cookieBanner";
     public const string ANALYTICSCOOKIENAME = "cookie_consent";
     public const string ANALYTICSCOOKIEACCEPTED = "accept";
     public const string ANALYTICSCOOKIEREJECTED = "reject";
     public const string CPDGOOGLEANALYTICSTAG = "CPD_GOOGLEANALYTICSTAG";
     public const string CLARITYPROJECTID = "CPD_CLARITY";
     public const string CPD_INSTRUMENTATION_CONNECTIONSTRING = "CPD_INSTRUMENTATION_CONNECTIONSTRING";
-    public const string CPDURLFORMAT = "/?pageName={0}&pageType={1}&sendingPage={2}&sendingPageType={3}";
-    public const int CONTENTLEVEL = 10;
     public const string CONTENTFULPREVIEWHOST = "preview.contentful.com";
     public const string ENVIRONMENT = "CPD_CONTENTFUL_ENVIRONMENT";
     public const string AZUREENVIRONMENT = "CPD_AZURE_ENVIRONMENT";
@@ -35,6 +24,5 @@ public static class SiteConstants
     public const string GOVUKHEADINGXL = "govuk-heading-xl";
     public const string GOVUKHEADINGL = "govuk-heading-l";
     public const string GOVUKHEADINGM = "govuk-heading-m";
-    public const string GOVUKHEADINGS = "govuk-heading-s";
     public const string LOADTESTAPPENVIRONMENT = "load-test";
 }
