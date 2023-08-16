821999a84099c8313f53cd8f1c15288c97e90db4
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-16 12:28:16 +0100

    Test controller exceptions get logged

--- Childrens-Social-Care-CPD-Tests/Controllers/ErrorController.ServerTests.cs
+++ Childrens-Social-Care-CPD-Tests/Controllers/ErrorController.ServerTests.cs
@@ -1,8 +1,11 @@
 ﻿using Childrens_Social_Care_CPD.Contentful.Models;
+using Childrens_Social_Care_CPD.Controllers;
 using Contentful.Core.Models;
 using Contentful.Core.Search;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using NSubstitute;
 using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
@@ -56,7 +59,26 @@ public class ErrorControllerServerTests
         var response = await _httpClient.GetAsync(url);
 
         // assert
+
         response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
         response.RequestMessage.RequestUri.AbsolutePath.Should().Be(url);
+        
+    }
+
+    [Test]
+    public async Task Exception_Will_Be_Logged()
+    {
+        // arrange
+        var exception = new TestException();
+        var logger = Substitute.For<ILogger<ErrorController>>();
+        _application.LoggerFactory.CreateLogger<ErrorController>().Returns(logger);
+        _application.CpdContentfulClient.GetEntries(Arg.Any<QueryBuilder<Content>>(), default).Throws(exception);
+        var url = "/something";
+
+        // act
+        await _httpClient.GetAsync(url);
+
+        // assert
+        logger.Received(1).LogError(exception, "Unhandled exception occurred");
     }
 }
