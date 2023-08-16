062a9fac8f56d17f13602b6cd682d26781ba60a1
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-16 10:46:05 +0100

    Update application css and create a powershell script to recreate

---
+++ Childrens-Social-Care-CPD/compile-sass.ps1
@@ -0,0 +1,3 @@
+npm ci
+npx sass --style=expanded --charset --source-map-urls=relative .\styles\scss\application.scss .\wwwroot\css\application.css
+npx sass --style=compressed --charset --source-map-urls=relative .\styles\scss\application.scss .\wwwroot\css\application.min.css
