f031934cf3132dbc60f43d5b7340e5c701117353
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-21 15:56:31 +0100

    Remove the old html helper for ContentLink.

--- Childrens-Social-Care-CPD-Tests/Contentful/CustomHtmlHelpersTests.cs
+++ Childrens-Social-Care-CPD-Tests/Contentful/CustomHtmlHelpersTests.cs
@@ -1,84 +1,16 @@
 ﻿using Childrens_Social_Care_CPD.Contentful;
 using Childrens_Social_Care_CPD.Contentful.Models;
 using Contentful.Core.Models;
-using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.Extensions.WebEncoders.Testing;
 using NSubstitute;
 using NUnit.Framework;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace Childrens_Social_Care_CPD_Tests.Contentful;
 
 public class CustomHtmlHelpersTests
 {
-    [Test]
-    public void ContentLink_Returns_Null_When_Not_Provided_A_HtmlHelper()
-    {
-        var actual = CustomHtmlHelpers.ContentLink(null, string.Empty, string.Empty);
-
-        actual.Should().BeNull();
-    }
-
-    [Test]
-    public void ContentLink_Builds_Internal_Content_Link_Correctly()
-    {
-        // arrange
-        var uri = "test_path";
-        var text = "Link text";
-        var expectedUri = $"<a class=\"HtmlEncode[[govuk-link]]\" href=\"HtmlEncode[[/{uri}]]\">HtmlEncode[[{text}]]</a>";
-        var stringWriter = new StringWriter();
-        var helper = Substitute.For<IHtmlHelper>();
-
-        // act
-        var result = helper.ContentLink(text, uri);
-        result.WriteTo(stringWriter, new HtmlTestEncoder());
-        var actual = stringWriter.ToString();
-
-        // assert
-        actual.Should().Be(expectedUri);
-    }
-
-    [Test]
-    public void ContentLink_Builds_Internal_Link_Correctly()
-    {
-        // arrange
-        var uri = "/test_path";
-        var text = "Link text";
-        var expectedUri = $"<a class=\"HtmlEncode[[govuk-link]]\" href=\"HtmlEncode[[{uri}]]\">HtmlEncode[[{text}]]</a>";
-        var stringWriter = new StringWriter();
-        var helper = Substitute.For<IHtmlHelper>();
-
-        // act
-        var result = helper.ContentLink(text, uri);
-        result.WriteTo(stringWriter, new HtmlTestEncoder());
-        var actual = stringWriter.ToString();
-
-        // assert
-        actual.Should().Be(expectedUri);
-    }
-
-    [Test]
-    public void ContentLink_Builds_External_Link_Correctly()
-    {
-        // arrange
-        var uri = "https://www.gov.uk/";
-        var text = "Link text";
-        var expectedUri = $"<a class=\"HtmlEncode[[govuk-link]]\" href=\"HtmlEncode[[{uri}]]\">HtmlEncode[[{text}]]</a>";
-        var stringWriter = new StringWriter();
-        var helper = Substitute.For<IHtmlHelper>();
-
-        // act
-        var result = helper.ContentLink(text, uri);
-        result.WriteTo(stringWriter, new HtmlTestEncoder());
-        var actual = stringWriter.ToString();
-
-        // assert
-        actual.Should().Be(expectedUri);
-    }
-
     [Test]
     public async Task RenderContentfulPartialAsync_Does_Not_Call_RenderPartial_When_Content_Item_Is_Null()
     {
