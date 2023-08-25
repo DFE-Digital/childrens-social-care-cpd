aac50c57c3730ab4d533426c19b820eab901522f
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-25 16:27:06 +0100

    Update footer.spec.ts

--- browser-tests/content-regression-tests/tests/footer.spec.ts
+++ browser-tests/content-regression-tests/tests/footer.spec.ts
@@ -11,19 +11,37 @@ test.describe('Page Footer', () => {
         ['Privacy policy', '/privacy'],
         ['Accessibility', '/accessibility'],
         ['Terms and conditions', '/termsconditions'],
-        ['Feedback', 'https://dferesearch.fra1.qualtrics.com/jfe/form/SV_bmcLDedq5wipeTA'],
-        ['Crown copyright', 'https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/'],
-        ['Open Government Licence v3.0', 'https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/'],
     ]
 
     for (const link of links) {
         test(`Footer contains link ${link[0]} that goes to ${link[1]}`, async ({ page }) => {
-            var responsePromise = page.waitForResponse(`**${link[1]}`)
+            var promise = page.waitForResponse(`**${link[1]}`)
             await page.getByRole('link', { name: link[0], exact: true }).click()
-            var response = await responsePromise
+            var response = await promise
 
             expect(response.ok()).toBeTruthy()
         })
     }
+
+    test.describe('External links @external', () => {
+    
+        const links = [
+            ['Feedback', 'https://dferesearch.fra1.qualtrics.com/jfe/form/SV_bmcLDedq5wipeTA'],
+            ['Open Government Licence v3.0', 'https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/'],
+            ['Crown copyright', 'https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/'],
+        ]
+        
+        for (const link of links) {
+            test(`'${link[0]}' goes to ${link[1]}`, async ({ page }) => {
+                var promise = page.waitForResponse(`**${link[1]}`)
+                await page.getByRole('link', { name: link[0], exact: true }).click()
+                var response = await promise
+                
+                expect(response.ok()).toBeTruthy()
+            })
+        }
+    })
+
+
 
 })
