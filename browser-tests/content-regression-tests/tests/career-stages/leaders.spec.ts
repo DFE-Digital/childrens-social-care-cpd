fa0c1f6b19cece35a6f0645ad87e425bb5d22f21
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-29 11:41:37 +0100

    Refactor to align by page
    Inherently content tests are fragile, but the business requirement to ensure the content doesn't accidentally change.

---
+++ browser-tests/content-regression-tests/tests/career-stages/leaders.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Leaders', () => {
+    test('User journey from homepage @journey', async ({ page }) => {
+        await page.goto('/')
+        await page.getByRole('link', { name: 'Leaders', exact: true }).click()
+
+        await expect(page.locator('h1', { hasText: /^Leaders$/ })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/leaders/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
+    })
+
+    test('User journey via menu @journey', async ({ page }) => {
+        await page.goto('/')
+        await page.getByLabel('Menu').getByRole('link', { name: 'Career information', exact: true }).click()
+        await page.getByRole('link', { name: 'Leaders', exact: true }).click()
+
+        await expect(page.locator('h1', { hasText: /^Leaders$/ })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/leaders/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
+    })
+
+    test.describe('Links', () => {
+        const links = [
+            ['Upon: aspirant directors programme', '/aspirant-directors-programme', 'Upon: aspirant directors programme'],
+            ['Upon: new directors programme', '/new-directors-programme', 'Upon: new directors programme'],
+            ['Pathway 4: practice leaders', '/pathway-4', 'Pathway 4: practice leaders'],
+            ['View all programmes', '/development-programmes', 'Child and family social work development programmes'],
+            ['Explore all roles', '/explore-roles', 'Roles in child and family social work'],
+        ]
+
+        for (const link of links) {
+            test(`Goes to the ${link[0]} page`, async ({ page }) => {
+                await page.goto('/leaders')
+                await page.getByRole('link', { name: link[0], exact: true }).click()
+                await expect(page).toHaveURL(new RegExp(`.*${link[1]}`))
+                await expect(page.locator('h1', { hasText: new RegExp(`^${link[2]}$`) })).toBeVisible()
+            })
+        }
+    })
+})
