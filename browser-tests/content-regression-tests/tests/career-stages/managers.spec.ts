f4a8e4fa97a4f671f8093df108cc70e06b3ef203
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-29 11:41:37 +0100

    Refactor to align by page
    Inherently content tests are fragile, but the business requirement to ensure the content doesn't accidentally change.

---
+++ browser-tests/content-regression-tests/tests/career-stages/managers.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Managers', () => {
+    test('User journey from homepage @journey', async ({ page }) => {
+        await page.goto('/')
+        await page.getByRole('link', { name: 'Managers', exact: true }).click()
+
+        await expect(page.locator('h1', { hasText: /^Managers$/ })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/managers/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
+    })
+
+    test('User journey via menu @journey', async ({ page }) => {
+        await page.goto('/')
+        await page.getByLabel('Menu').getByRole('link', { name: 'Career information', exact: true }).click()
+        await page.getByRole('link', { name: 'Managers', exact: true }).click()
+
+        await expect(page.locator('h1', { hasText: /^Managers$/ })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/managers/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
+    })
+
+    test.describe('Links', () => {
+        const links = [
+            ['Pathway 2: middle managers', '/pathway-2', 'Pathway 2: middle managers'],
+            ['Pathway 3: heads of service', '/pathway-3', 'Pathway 3: heads of service'],
+            ['View all programmes', '/development-programmes', 'Child and family social work development programmes'],
+            ['Explore all roles', '/explore-roles', 'Roles in child and family social work'],
+        ]
+
+        for (const link of links) {
+            test(`Goes to the ${link[0]} page`, async ({ page }) => {
+                await page.goto('/managers')
+                await page.getByRole('link', { name: link[0], exact: true }).click()
+                await expect(page).toHaveURL(new RegExp(`.*${link[1]}`))
+                await expect(page.locator('h1', { hasText: new RegExp(`^${link[2]}$`) })).toBeVisible()
+            })
+        }
+    })
+})
