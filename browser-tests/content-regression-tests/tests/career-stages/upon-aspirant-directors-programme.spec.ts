508a0dc70a305afce351ec0ea1e67449e9ed28a0
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-08-29 16:17:07 +0100

    Regression tests (wip)

--- browser-tests/content-regression-tests/tests/career-stages/upon-aspirant-directors-programme.spec.ts
+++ browser-tests/content-regression-tests/tests/career-stages/upon-aspirant-directors-programme.spec.ts
@@ -1,37 +1,36 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Pathway 1: practice supervisors', () => {
+test.describe('Upon: aspirant directors programme', () => {
     test('User journey from homepage @journey', async ({ page }) => {
         await page.goto('/')
-        await page.getByRole('link', { name: 'Experienced practitioners', exact: true }).click()
-        await page.getByRole('link', { name: 'Pathway 1: practice supervisors', exact: true }).click()
+        await page.getByRole('link', { name: 'Leaders', exact: true }).click()
+        await page.getByRole('link', { name: 'Upon: aspirant directors programme', exact: true }).click()
 
-        await expect(page.locator('h1', { hasText: /^Pathway 1: practice supervisors$/ })).toBeVisible()
-        await expect(page).toHaveURL(/.*\/pathway-1/)
+        await expect(page.locator('h1', { hasText: /^Upon: aspirant directors programme$/ })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/aspirant-directors-programme/)
         await expect(page.locator('#mmi-developmentProgrammes')).toHaveClass(/dfe-header__navigation-item--current/)
     })
 
     test('User journey via menu @journey', async ({ page }) => {
         await page.goto('/')
         await page.getByLabel('Menu').getByRole('link', { name: 'Career information', exact: true }).click()
-        await page.getByRole('link', { name: 'Experienced practitioners', exact: true }).click()
-        await page.getByRole('link', { name: 'Pathway 1: practice supervisors', exact: true }).click()
+        await page.getByRole('link', { name: 'Leaders', exact: true }).click()
+        await page.getByRole('link', { name: 'Upon: aspirant directors programme', exact: true }).click()
 
-        await expect(page.locator('h1', { hasText: /^Pathway 1: practice supervisors$/ })).toBeVisible()
-        await expect(page).toHaveURL(/.*\/pathway-1/)
+        await expect(page.locator('h1', { hasText: /^Upon: aspirant directors programme$/ })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/aspirant-directors-programme/)
         await expect(page.locator('#mmi-developmentProgrammes')).toHaveClass(/dfe-header__navigation-item--current/)
     })
 
     test.describe('External links @external', () => {
         const links = [
-            ['Frontline', 'https://thefrontline.org.uk/develop-your-career/pathways-programme/'],
-            ['Find out more about Pathway 1 including how to apply', 'https://thefrontline.org.uk/develop-your-career/pathways-programme/'],
+            ['Find out more about the upon: aspirant directors programme', 'https://uponleaders.co.uk/aspirant-directors-programme/'],
             ['Social work post-qualifying standards', 'https://www.gov.uk/government/publications/knowledge-and-skills-statements-for-child-and-family-social-work'],
         ]
         
         for (const link of links) {
             test(`'${link[0]}' goes to ${link[1]}`, async ({ page }) => {
-                await page.goto('/pathway-1')
+                await page.goto('/aspirant-directors-programme')
                 var promise = page.waitForResponse(`**${link[1]}`)
                 await page.getByRole('link', { name: link[0], exact: true }).click()
                 var response = await promise
