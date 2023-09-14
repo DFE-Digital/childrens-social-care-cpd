685b00883c6b4915ff2a8ca7e555f8490fe6b5b4
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-09-14 17:26:13 +0100

    Update FeatureConfigurationPollerTests.cs

--- Childrens-Social-Care-CPD-Tests/Configuration/FeatureConfigurationPollerTests.cs
+++ Childrens-Social-Care-CPD-Tests/Configuration/FeatureConfigurationPollerTests.cs
@@ -10,7 +10,6 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace Childrens_Social_Care_CPD_Tests.Configuration;
 
 public class FeatureConfigurationPollerTests
@@ -23,15 +22,17 @@ public class FeatureConfigurationPollerTests
     public void Setup()
     {
         _logger = Substitute.For<ILogger<FeatureConfigurationPoller>>();
+
         _applicationConfiguration = Substitute.For<IApplicationConfiguration>();
         _featureConfigurationUpdater = Substitute.For<IFeatureConfigurationUpdater>();
     }
 
-    [Test]
-    public async Task Poller_Calls_Updater()
+    [TestCase(500)]
+    [TestCase(1000)]
+    public async Task Poller_Calls_Updater_At_Specified_Interval(int interval)
     {
         // arrange
-        _applicationConfiguration.FeaturePollingInterval.Returns(1);
+        _applicationConfiguration.FeaturePollingInterval.Returns(interval);
         var featureConfigurationPoller = new FeatureConfigurationPoller(
             _logger,
             _applicationConfiguration,
@@ -42,12 +43,12 @@ public class FeatureConfigurationPollerTests
         using (var cancellationTokenSource = new CancellationTokenSource())
         {
             var task = featureConfigurationPoller.StartAsync(cancellationTokenSource.Token);
-            await Task.Delay(50);
+            await Task.Delay((int)(interval * 1.1));
             cancellationTokenSource.Cancel();
             task.Wait();
         }
 
         // assert
-        await _featureConfigurationUpdater.Received().UpdateFeaturesAsync(Arg.Any<CancellationToken>());
+        await _featureConfigurationUpdater.Received(1).UpdateFeaturesAsync(Arg.Any<CancellationToken>());
     }
 }
