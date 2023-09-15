f116ca6594b764ceb9a153256a6ee53f9a88fc99
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-09-15 14:42:09 +0100

    Refactor the web app setup into some extension methods & change the AppInsights configuration so that it works properly.

--- Childrens-Social-Care-CPD/Program.cs
+++ Childrens-Social-Care-CPD/Program.cs
@@ -1,59 +1,11 @@
 using Childrens_Social_Care_CPD;
-using Childrens_Social_Care_CPD.Configuration;
-using Childrens_Social_Care_CPD.Contentful;
-using Childrens_Social_Care_CPD.Contentful.Renderers;
-using Contentful.AspNetCore;
-using Contentful.Core.Configuration;
-using Microsoft.ApplicationInsights.AspNetCore.Extensions;
-using Microsoft.Extensions.Logging.AzureAppServices;
 using System.Diagnostics.CodeAnalysis;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.ConfigureLogging(logging => logging.AddAzureWebAppDiagnostics())
-.ConfigureServices(serviceCollection => serviceCollection
-    .Configure<AzureFileLoggerOptions>(options =>
-    {
-        options.FileName = "azure-diagnostics-";
-        options.FileSizeLimit = 50 * 1024;
-        options.RetainedFileCountLimit = 5;
-    }).Configure<AzureBlobLoggerOptions>(options =>
-    {
-        options.BlobName = "log.txt";
-    })
-);
+builder.AddDependencies();
+builder.AddFeatures();
 
-// Add services to the container.
-var applicationConfiguration = new ApplicationConfiguration();
-
-builder.Services.AddResponseCompression();
-builder.Services.AddControllersWithViews();
-builder.Services.AddContentful(ContentfulConfiguration.GetContentfulConfiguration(builder.Configuration, applicationConfiguration));
-builder.Services.AddTransient<IContentTypeResolver, EntityResolver>();
-builder.Services.AddTransient<ICpdContentfulClient, CpdContentfulClient>();
-builder.Services.AddSingleton<IApplicationConfiguration>(applicationConfiguration);
-builder.Services.AddSingleton<ICookieHelper, CookieHelper>();
-builder.Services.AddTransient<IFeaturesConfig, FeaturesConfig>();
-builder.Services.AddTransient<IFeaturesConfigUpdater, FeaturesConfigUpdater>();
-builder.Services.AddHostedService<FeaturesConfigBackgroundService>();
-
-System.Reflection.Assembly.GetExecutingAssembly()
-    .GetTypes()
-    .Where(item => item.GetInterfaces().Where(i => i.IsGenericType).Any(i => i.GetGenericTypeDefinition() == typeof(IRenderer<>)) && !item.IsAbstract && !item.IsInterface)
-    .ToList()
-    .ForEach(assignedTypes =>
-    {
-        var serviceType = assignedTypes.GetInterfaces().First(i => i.GetGenericTypeDefinition() == typeof(IRenderer<>));
-        builder.Services.AddScoped(serviceType, assignedTypes);
-    });
-
-var options = new ApplicationInsightsServiceOptions
-{
-    ConnectionString = applicationConfiguration.AppInsightsConnectionString
-};
-
-builder.Services.AddApplicationInsightsTelemetry(options: options);
 var app = builder.Build();
-
 app.UseResponseCompression();
 app.UseExceptionHandler("/error/error");
 app.UseStatusCodePagesWithReExecute("/error/{0}");
