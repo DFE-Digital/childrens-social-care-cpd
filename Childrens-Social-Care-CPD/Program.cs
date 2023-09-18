fc6d793064ccc56833478e7228f5462f4e218577
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-09-18 12:24:24 +0100

    Fetch the features at app startup to ensure they're loaded and available before serving requests

--- Childrens-Social-Care-CPD/Program.cs
+++ Childrens-Social-Care-CPD/Program.cs
@@ -1,4 +1,5 @@
 using Childrens_Social_Care_CPD;
+using Childrens_Social_Care_CPD.Configuration;
 using System.Diagnostics.CodeAnalysis;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,14 @@ builder.AddDependencies();
 builder.AddFeatures();
 
 var app = builder.Build();
+
+// Ensure the features are fetched before starting the app
+var updater = app.Services.GetService<IFeaturesConfigUpdater>();
+using (CancellationTokenSource cts = new CancellationTokenSource())
+{
+    await updater.UpdateFeaturesAsync(cts.Token);
+}
+
 app.UseResponseCompression();
 app.UseExceptionHandler("/error/error");
 app.UseStatusCodePagesWithReExecute("/error/{0}");
