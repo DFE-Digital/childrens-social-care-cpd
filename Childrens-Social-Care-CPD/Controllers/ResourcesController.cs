8ea913e4909f333603ab79db8e45dbacf7ac37b8
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-09-25 14:52:07 +0100

    Resources search (wip)

---
+++ Childrens-Social-Care-CPD/Controllers/ResourcesController.cs
@@ -0,0 +1,96 @@
+﻿using Childrens_Social_Care_CPD.Contentful;
+using Childrens_Social_Care_CPD.Contentful.Models;
+using Childrens_Social_Care_CPD.Models;
+using Contentful.Core.Models;
+using Contentful.Core.Search;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Childrens_Social_Care_CPD.Controllers;
+
+public record TagInfo(string Category, string DisplayName, string TagName);
+
+public class ResourcesQuery
+{
+    public int[] Tags { get; set; }
+    public int Page { get; set; } = 1;
+}
+
+public partial class ResourcesController : Controller
+{
+    private readonly ILogger<ResourcesController> _logger;
+    private readonly ICpdContentfulClient _cpdClient;
+
+    private static readonly List<TagInfo> _tagInfos = new () { 
+        new TagInfo("Type", "Case studies", "caseStudies"),
+        new TagInfo("Type", "CPD", "cpd"),
+        new TagInfo("Type", "Direct tools", "directTools"),
+        new TagInfo("Type", "Knowledge articles", "knowledgeArticles"),
+        new TagInfo("Career stage", "Practitioner", "practitioner"),
+        new TagInfo("Career stage", "Experienced practitioner", "experiencedPractitioner"),
+        new TagInfo("Career stage", "Manager", "manager"),
+    };
+
+    private static readonly IEnumerable<string> _allTags = _tagInfos.Select(x => x.TagName);
+
+    public ResourcesController(ILogger<ResourcesController> logger, ICpdContentfulClient cpdClient)
+    {
+        _logger = logger;
+        _cpdClient = cpdClient;
+    }
+
+    private IEnumerable<string> GetQueryTags(int[] tags)
+    {
+        if (tags.Length == 0)
+        {
+            return _allTags;
+        }
+
+        try
+        {
+            return tags.Select(x => _tagInfos[x].TagName);
+        }
+        catch
+        {
+            _logger.LogWarning("Passed tag values do not match known values: {Passed Values}", tags);
+            throw;
+        }
+    }
+
+    private Task<ContentfulCollection<Content>> FetchPagesContentAsync(int[] tags, int skip = 0, int limit = 5)
+    {
+        // TODO: we need to have the resources content model
+        var queryBuilder = QueryBuilder<Content>.New
+            .ContentTypeIs("content")
+            .Include(1)
+            .FieldIncludes("metadata.tags.sys.id", GetQueryTags(tags))
+            .OrderBy("-sys.createdAt")
+            .Skip(skip)
+            .Limit(limit);
+        
+        return _cpdClient.GetEntries(queryBuilder);
+    }
+
+    [Route("resources", Name = "Resource")]
+    [HttpGet]
+    public async Task<IActionResult> Index([FromQuery] ResourcesQuery query, bool preferencesSet = false)
+    {
+        // TODO: we should probably query for a resources object to enable content management of the banner
+        query ??= new ResourcesQuery();
+        query.Tags ??= Array.Empty<int>();
+
+        var pageSize = 8;
+        var page = query.Page;
+        page = Math.Max(page, 1);
+        var skip = (page - 1) * pageSize;
+
+        var contentCollection = await FetchPagesContentAsync(query.Tags, skip, pageSize);
+        var totalPages = (int)Math.Ceiling((decimal)contentCollection.Total / pageSize);
+        page = Math.Min(page, totalPages);
+
+        var contextModel = new ContextModel(string.Empty, "Resources", "Resources", "Resources", true, preferencesSet);
+        ViewData["ContextModel"] = contextModel;
+
+        var viewModel = new ResourcesListViewModel(contentCollection, _tagInfos, query.Tags, page, totalPages, contentCollection.Total);
+        return View(viewModel);
+    }
+}
---
+++ Childrens-Social-Care-CPD/Models/ResourcesListViewModel.cs
@@ -0,0 +1,7 @@
+﻿using Childrens_Social_Care_CPD.Contentful.Models;
+using Childrens_Social_Care_CPD.Controllers;
+using Contentful.Core.Models;
+
+namespace Childrens_Social_Care_CPD.Models;
+
+public record ResourcesListViewModel(ContentfulCollection<Content> Content, IEnumerable<TagInfo> TagInfos, int[] SelectedTags, int CurrentPage = 0, int TotalPages = 0, int TotalResults = 0);
