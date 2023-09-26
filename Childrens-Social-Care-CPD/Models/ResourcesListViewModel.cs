ffe09e9336e64dc9b08899cf1e9b75059b55bb41
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-09-26 08:38:03 +0100

    Fetch the page content and display if available

--- Childrens-Social-Care-CPD/Models/ResourcesListViewModel.cs
+++ Childrens-Social-Care-CPD/Models/ResourcesListViewModel.cs
@@ -4,4 +4,4 @@ using Contentful.Core.Models;
 
 namespace Childrens_Social_Care_CPD.Models;
 
-public record ResourcesListViewModel(ContentfulCollection<Content> Content, IEnumerable<TagInfo> TagInfos, int[] SelectedTags, int CurrentPage = 0, int TotalPages = 0, int TotalResults = 0);
+public record ResourcesListViewModel(Content Content, ContentfulCollection<Content> SearchResults, IEnumerable<TagInfo> TagInfos, int[] SelectedTags, int CurrentPage = 0, int TotalPages = 0, int TotalResults = 0);
