b2d75472ff88898d6cb2afc094578129e163833d
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-09-27 17:10:34 +0100

    Update GovUkPaginationTagHelper.cs

--- Childrens-Social-Care-CPD/TagHelpers/GovUkPaginationTagHelper.cs
+++ Childrens-Social-Care-CPD/TagHelpers/GovUkPaginationTagHelper.cs
@@ -63,8 +63,6 @@ public class GovUkPaginationTagHelper : TagHelper
                         ul.InnerHtml.AppendHtml(GenerateEllipses());
                         break;
                     }
-                default:
-                        continue;
             }
         }
 
