4905121493dc4edbe054e76d0c3791efbcdf91ac
nileshgnagepatil <nileshgnagepatil@outlook.com>
2023-10-17 08:38:09 +0100

    Support for Image resource.

---
+++ Childrens-Social-Care-CPD/Contentful/Models/ImageResource.cs
@@ -0,0 +1,9 @@
+﻿using Contentful.Core.Models;
+
+namespace Childrens_Social_Care_CPD.Contentful.Models;
+
+public class ImageResource : IContent
+{
+    public string Id { get; set; }
+    public Asset Image { get; set; }
+}
