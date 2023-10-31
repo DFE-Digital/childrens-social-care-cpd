44dddf8d2e215713fb3d2d786ae7d8bd2cafaabe
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-10-31 15:48:18 +0000

    Update the resources menu item and feature control the whole of resources

--- Childrens-Social-Care-CPD/Configuration/Features.cs
+++ Childrens-Social-Care-CPD/Configuration/Features.cs
@@ -2,5 +2,6 @@
 
 public static class Features
 {
+    public const string ResourcesAndLearning = "resources-learning";
     public const string ResourcesUseDynamicTags = "resources-search-use-dynamic-tags";
 }
