0769a5807d5dee7c7a74e1c1aed3ebf4bdf81ddd
cairnsj <51908793+cairnsj@users.noreply.github.com>
2023-11-07 11:19:46 +0000

    Move the properties list into a tag helper

---
+++ Childrens-Social-Care-CPD/TagHelpers/CpdPropertiesList.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+
+namespace Childrens_Social_Care_CPD.TagHelpers;
+
+[HtmlTargetElement(TagName)]
+[OutputElementHint("ul")]
+public class CpdPropertiesList : TagHelper
+{
+    internal const string TagName = "cpd-properties-list";
+
+    [HtmlAttributeName("items")]
+    public IReadOnlyDictionary<string, string> Items { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (Items == null || Items.Count == 0 )
+        {
+            output.SuppressOutput();
+            return;
+        }
+
+        output.TagName = "ul";
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.AddClass("course-details", HtmlEncoder.Default);
+        output.AddClass("govuk-list", HtmlEncoder.Default);
+
+        foreach (var item in Items)
+        {
+            var span = new TagBuilder("span");
+            span.AddCssClass("govuk-!-font-size-16");
+            span.InnerHtml.AppendHtml($"{item.Key}:&nbsp{item.Value}");
+
+            var li = new TagBuilder("li");
+            li.InnerHtml.AppendHtml(span);
+
+            output.Content.AppendHtml(li);
+        }
+    }
+}
