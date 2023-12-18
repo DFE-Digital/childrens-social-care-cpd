f5eaf50300a7be75a92e25935f4277288fa1384c
Al West <al@tsew.net>
2023-12-18 14:10:49 +0000

    Add var for search sku

--- Terraform/ai-search.tf
+++ Terraform/ai-search.tf
@@ -2,6 +2,6 @@ resource "azurerm_search_service" "ai-search" {
   name                = var.ai_search_service_name[terraform.workspace]
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
-  sku                 = "free"
+  sku                 = var.ai_search_service_sku[terraform.workspace]
   tags                = data.azurerm_resource_group.rg.tags
 }
