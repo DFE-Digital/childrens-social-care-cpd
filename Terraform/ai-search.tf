2764940e418e18b034d99c37b7e1295e6705a772
Al West <al@tsew.net>
2023-12-18 13:52:41 +0000

    Add Terraform for AI Search Service

---
+++ Terraform/ai-search.tf
@@ -0,0 +1,9 @@
+resource "azurerm_search_service" "ai-search" {
+  name                = var.ai_search_service_name[terraform.workspace]
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = "free"
+  tags                = data.azurerm_resource_group.rg.tags
+
+  
+}
