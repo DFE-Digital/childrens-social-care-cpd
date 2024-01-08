00b1175f4242b3e51527dacdc8747ee79b414b77
killij <51908793+killij@users.noreply.github.com>
2024-01-08 09:00:07 +0000

    Display resource properties in search results and resource page

--- Childrens-Social-Care-CPD/Controllers/ResourcesController.cs
+++ Childrens-Social-Care-CPD/Controllers/ResourcesController.cs
@@ -1,5 +1,7 @@
 ﻿using Childrens_Social_Care_CPD.Configuration;
+using Childrens_Social_Care_CPD.Contentful.Models;
 using Childrens_Social_Care_CPD.DataAccess;
+using Childrens_Social_Care_CPD.GraphQL.Queries;
 using Childrens_Social_Care_CPD.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -34,6 +36,37 @@ public class ResourcesController : Controller
         _resourcesRepository = resourcesRepository;
     }
 
+    private Dictionary<string, string> GetProperties(GetContentTags.ResponseType tags, Content content)
+    {
+        var properties = new Dictionary<string, string>()
+        {
+            { "Published", content.Sys.CreatedAt?.ToString("dd MMMM yyyy") },
+            { "Last updated", content.Sys.UpdatedAt?.ToString("dd MMMM yyyy") }
+        };
+
+        void AddProperty(string key, string prefix)
+        {
+            var propertyTags = tags.ContentCollection.Items.First().ContentfulMetaData.Tags.Where(x => x.Name.StartsWith($"{prefix}:"));
+            if (propertyTags.Count() > 0)
+            {
+                var value = string.Join(", ", propertyTags.Select(x => x.Name.Split(":")[1].Trim(' ')));
+                properties.Add(key, value);
+            }
+        }
+
+        AddProperty("From", "Resource provider");
+        AddProperty("Resource type", "Format");
+        AddProperty("Topic", "Topic");
+        AddProperty("Career stage", "Career stage");
+
+        if (content.EstimatedReadingTime.HasValue && content.EstimatedReadingTime > 0)
+        {
+            properties.Add("Estimated reading time", $"{content.EstimatedReadingTime} mins");
+        }
+
+        return properties;
+    }
+
     [Route("resources-learning/{*pagename:regex(^[[0-9a-z]](\\/?[[0-9a-z\\-]])*\\/?$)}")]
     public async Task<IActionResult> Index(string pageName = "home", bool preferenceSet = false, CancellationToken cancellationToken = default)
     {
@@ -49,19 +82,6 @@ public class ResourcesController : Controller
             return NotFound();
         }
 
-        var properties = new Dictionary<string, string>(tags.ContentCollection.Items.First().ContentfulMetaData.Tags.Where(x => x.Name.StartsWith("Resource:")).Select(x =>
-        {
-            var property = x.Name[9..];
-            var tokens = property.Split('=');
-            return tokens.Length > 1
-                ? KeyValuePair.Create(tokens[0].Trim(' '), tokens[1].Trim(' '))
-                : KeyValuePair.Create(property, string.Empty);
-        }))
-        {
-            { "Published", content.Sys.CreatedAt?.ToString("dd MMMM yyyy") },
-            { "Last updated", content.Sys.UpdatedAt?.ToString("dd MMMM yyyy") }
-        };
-
         var contextModel = new ContextModel(
             Id: content.Id,
             Title: content.Title,
@@ -73,7 +93,7 @@ public class ResourcesController : Controller
 
         ViewData["ContextModel"] = contextModel;
         ViewData["StateModel"] = new StateModel();
-        ViewData["Properties"] = properties;
+        ViewData["Properties"] = GetProperties(tags, content);
 
         return View("Resource", content);
     }
---
+++ Childrens-Social-Care-CPD/Models/SearchResultViewModel.cs
@@ -0,0 +1,7 @@
+﻿using Azure.Search.Documents.Models;
+using Childrens_Social_Care_CPD.Core.Resources;
+using Childrens_Social_Care_CPD.Search;
+
+namespace Childrens_Social_Care_CPD.Models;
+
+public record SearchResultViewModel(SearchResult<CpdDocument> SearchResult, IReadOnlyDictionary<string, TagInfo> TagMap);
