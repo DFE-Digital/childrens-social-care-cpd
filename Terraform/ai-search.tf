a016d3bfaa717972ea6684f1750752aa7a407d81
Al West <al@tsew.net>
2024-01-11 16:03:18 +0000

    Only create AI-Search in the Test, Pre-Prod and Prod environments

--- Terraform/ai-search.tf
+++ Terraform/ai-search.tf
@@ -2,9 +2,11 @@
 locals {
   with_basic_aisearch_count = can(regex("^Prod|Load", terraform.workspace)) ? 1 : 0
   with_basic_aisearch_bool  = can(regex("^Prod|Load", terraform.workspace)) ? true : false
+  create_aisearch_count     = can(regex("^Test|Prod", terraform.workspace)) ? 1 : 0
 }
 
 resource "azurerm_search_service" "ai-search" {
+  count                         = local.create_aisearch_count
   name                          = var.ai_search_service_name[terraform.workspace]
   resource_group_name           = data.azurerm_resource_group.rg.name
   location                      = data.azurerm_resource_group.rg.location
