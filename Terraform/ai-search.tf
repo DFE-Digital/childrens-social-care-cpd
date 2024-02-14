78adbaae6f46ea76a0245595257dfda5774625c3
Al West <al@tsew.net>
2024-02-14 17:16:01 +0000

    WIP: New Vault and IP Whitelist moved to GitHub Var

--- Terraform/ai-search.tf
+++ Terraform/ai-search.tf
@@ -14,3 +14,13 @@ resource "azurerm_search_service" "ai-search" {
   sku                           = var.ai_search_service_sku[terraform.workspace]
   tags                          = data.azurerm_resource_group.rg.tags
 }
+
+# The keyvault to be used for AI Search secrets
+resource "azurerm_key_vault" "ai-search-kv" {
+  name                = var.search_indexer_key_vault_name[terraform.workspace]
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
+  tags                = data.azurerm_resource_group.rg.tags
+}
