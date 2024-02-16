2c498b3e1ca9b4eaa57ee8cec71714ec202f0ca3
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2024-02-16 14:36:16 +0000

    updated develop-social-work-practice.spec

--- browser-tests/content-regression-tests/tests/career-stages/develop-social-work-practice.spec.ts
+++ browser-tests/content-regression-tests/tests/career-stages/develop-social-work-practice.spec.ts
@@ -7,8 +7,9 @@ test.describe('Practitioner', () => {
         await page.getByLabel('Menu').getByRole('link', { name: 'Career stages', exact: true }).click()
         await page.getByRole('link', { name: 'Practitioner', exact: true }).click()
         
-        await expect(page.locator('h1', { hasText: "Practitioner" })).toBeVisible()
+        await expect(page.locator('h1', { hasText: /^Practitioner$/ })).toBeVisible()
         await expect(page).toHaveURL(/.*\/practitioner/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
         
     })
 
@@ -21,8 +22,9 @@ test.describe('Experienced Practitioner', () => {
         await page.getByRole('link', { name: 'Practitioner', exact: true }).click()
         await page.getByRole('link', { name: 'Experienced practitioner', exact: true }).click()
 
-        await expect(page.locator('h1', { hasText: "Experienced practitioner" })).toBeVisible()
+        await expect(page.locator('h1', { hasText: /^Experienced practitioner$/ })).toBeVisible()
         await expect(page).toHaveURL(/.*experienced-practitioner/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
         
     })
 
@@ -35,8 +37,9 @@ test.describe('Manager', () => {
         await page.getByLabel('Menu').getByRole('link', { name: 'Career stages', exact: true }).click()
         await page.getByRole('link', { name: 'Manager', exact: true }).click()
         
-        await expect(page.locator('h1', { hasText: "Manager" })).toBeVisible()
+        await expect(page.locator('h1', { hasText: /^Manager$/ })).toBeVisible()
         await expect(page).toHaveURL(/.*\/manager/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
         
     })
 
@@ -49,8 +52,9 @@ test.describe('Senior Manager', () => {
         await page.getByLabel('Menu').getByRole('link', { name: 'Career stages', exact: true }).click()
         await page.getByRole('link', { name: 'Senior manager', exact: true }).click()
         
-        await expect(page.locator('h1', { hasText: "Senior manager" })).toBeVisible()
+        await expect(page.locator('h1', { hasText: /^Senior manager$/ })).toBeVisible()
         await expect(page).toHaveURL(/.*\/senior-manager/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
         
     })
 
@@ -63,8 +67,9 @@ test.describe('Senior Leader', () => {
         await page.getByLabel('Menu').getByRole('link', { name: 'Career stages', exact: true }).click()
         await page.getByRole('link', { name: 'Senior leader', exact: true }).click()
         
-        await expect(page.locator('h1', { hasText: "Senior leader" })).toBeVisible()
+        await expect(page.locator('h1', { hasText: /^Senior Leader$/ })).toBeVisible()
         await expect(page).toHaveURL(/.*\/senior-leader/)
+        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
         
     })
 
