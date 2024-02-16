c6a6e481bf10f2332036e8dfc4ba7159ae38de5e
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2024-02-16 14:25:15 +0000

    updated develop-social-work-practice.spec

--- browser-tests/content-regression-tests/tests/career-stages/develop-social-work-practice.spec.ts
+++ browser-tests/content-regression-tests/tests/career-stages/develop-social-work-practice.spec.ts
@@ -1,44 +1,71 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Experienced Practitioners', () => {
-
-    test('User journey from homepage @journey', async ({ page }) => {
-        await page.goto('/')
-        await page.getByRole('link', { name: 'Practitioners', exact: true }).click()
-        await page.getByRole('link', { name: 'Develop your social work practice', exact: true }).click()
-
-        await expect(page.locator('h1', { hasText: /^Develop your social work practice$/ })).toBeVisible()
-        await expect(page).toHaveURL(/.*\/develop-social-work-practice/)
-        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
-    })
+test.describe('Practitioner', () => {
 
     test('User journey via menu @journey', async ({ page }) => {
         await page.goto('/')
         await page.getByLabel('Menu').getByRole('link', { name: 'Career stages', exact: true }).click()
-        await page.getByRole('link', { name: 'Practitioners', exact: true }).click()
-        await page.getByRole('link', { name: 'Develop your social work practice', exact: true }).click()
-
-        await expect(page.locator('h1', { hasText: /^Develop your social work practice$/ })).toBeVisible()
-        await expect(page).toHaveURL(/.*\/develop-social-work-practice/)
-        await expect(page.locator('#mmi-career')).toHaveClass(/dfe-header__navigation-item--current/)
-    })
-
-    test.describe('External links @external', () => {
-        const links = [
-            ['Find out more about the types of CPD you can do', 'https://www.socialworkengland.org.uk/cpd/what-counts-as-cpd/'],
-            ['Find out more about CPD requirements', 'https://www.socialworkengland.org.uk/cpd/'],
-            ['Social work post-qualifying standards', 'https://www.gov.uk/government/publications/knowledge-and-skills-statements-for-child-and-family-social-work'],
-        ]
+        await page.getByRole('link', { name: 'Practitioner', exact: true }).click()
         
-        for (const link of links) {
-            test(`'${link[0]}' goes to ${link[1]}`, async ({ page }) => {
-                await page.goto('/develop-social-work-practice')
-                var promise = page.waitForResponse(`**${link[1]}`)
-                await page.getByRole('link', { name: link[0], exact: true }).click()
-                var response = await promise
-                
-                expect(response.ok()).toBeTruthy()
-            })
-        }
+        await expect(page.locator('h1', { hasText: "Practitioner" })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/practitioner/)
+        
     })
+
+})
+
+test.describe('Experienced Practitioner', () => {
+
+    test('User journey from homepage @journey', async ({ page }) => {
+        await page.goto('/')
+        await page.getByRole('link', { name: 'Practitioner', exact: true }).click()
+        await page.getByRole('link', { name: 'Experienced practitioner', exact: true }).click()
+
+        await expect(page.locator('h1', { hasText: "Experienced practitioner" })).toBeVisible()
+        await expect(page).toHaveURL(/.*experienced-practitioner/)
+        
+    })
+
+})
+
+test.describe('Manager', () => {
+
+    test('User journey via menu @journey', async ({ page }) => {
+        await page.goto('/')
+        await page.getByLabel('Menu').getByRole('link', { name: 'Career stages', exact: true }).click()
+        await page.getByRole('link', { name: 'Manager', exact: true }).click()
+        
+        await expect(page.locator('h1', { hasText: "Manager" })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/manager/)
+        
+    })
+
+})
+
+test.describe('Senior Manager', () => {
+
+    test('User journey via menu @journey', async ({ page }) => {
+        await page.goto('/')
+        await page.getByLabel('Menu').getByRole('link', { name: 'Career stages', exact: true }).click()
+        await page.getByRole('link', { name: 'Senior manager', exact: true }).click()
+        
+        await expect(page.locator('h1', { hasText: "Senior manager" })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/senior-manager/)
+        
+    })
+
+})
+
+test.describe('Senior Leader', () => {
+
+    test('User journey via menu @journey', async ({ page }) => {
+        await page.goto('/')
+        await page.getByLabel('Menu').getByRole('link', { name: 'Career stages', exact: true }).click()
+        await page.getByRole('link', { name: 'Senior leader', exact: true }).click()
+        
+        await expect(page.locator('h1', { hasText: "Senior leader" })).toBeVisible()
+        await expect(page).toHaveURL(/.*\/senior-leader/)
+        
+    })
+
 })
