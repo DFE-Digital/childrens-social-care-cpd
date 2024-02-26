043eb9c33dd64b0368644a717d49245a1713d10b
Al West <al@tsew.net>
2024-02-26 13:36:35 +0000

    Fix Terraform format

--- Terraform/application.tf
+++ Terraform/application.tf
@@ -46,7 +46,7 @@ resource "azurerm_linux_web_app" "linux-web-app" {
     vnet_route_all_enabled = true
   }
 
- virtual_network_subnet_id = azurerm_subnet.appstorage.id
+  virtual_network_subnet_id = azurerm_subnet.appstorage.id
 
   logs {
     http_logs {
