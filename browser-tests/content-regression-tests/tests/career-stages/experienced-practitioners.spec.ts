2040c9534def48e22e20a1c4dcfbc1ad205f45ae
killij <51908793+killij@users.noreply.github.com>
2024-03-01 11:34:46 +0000

    Update regression tests

--- browser-tests/content-regression-tests/tests/career-stages/experienced-practitioners.spec.ts
+++ browser-tests/content-regression-tests/tests/career-stages/experienced-practitioners.spec.ts
@@ -27,13 +27,14 @@ test.describe('Experienced Practitioners', () => {
         const links = [
             ['Pathway 1: practice supervisors', '/pathway-1', 'Pathway 1: practice supervisors'],
             ['Pathway 2: middle managers', '/pathway-2', 'Pathway 2: middle managers'],
-            ['Explore roles', '/explore-roles', 'Explore roles in child and family social work'],
+            ['View all programmes', '/development-programmes', 'Child and family social work development programmes'],
+            ['Explore roles', '/explore-roles', 'Roles in child and family social work'],
         ]
 
         for (const link of links) {
             test(`Goes to the ${link[0]} page`, async ({ page }) => {
-                await page.goto('/experienced-practitioner')
-                await page.goto(link[1])
+                await page.goto('/career-stages/experienced-practitioner')
+                await page.getByRole('link', { name: link[0], exact: true }).last().click()
                 await expect(page).toHaveURL(new RegExp(`.*${link[1]}`))
                 await expect(page.locator('h1', { hasText: new RegExp(`^${link[2]}$`) })).toBeVisible()
             })
