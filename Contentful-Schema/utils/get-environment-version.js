7dfc45e3eab44131bd2448c0bab98ed263d012fc
Matt Barton <matt.barton@hippodigital.co.uk>
2024-04-18 13:41:43 +0100

    added util scripts from contentful poc

---
+++ Contentful-Schema/utils/get-environment-version.js
@@ -0,0 +1,17 @@
+import contentful from 'contentful';
+import minimist from 'minimist';
+
+const argv = minimist(process.argv.slice(2));
+
+var client = contentful.createClient({
+    accessToken: argv.token,
+    space: argv.space
+});
+
+client.getEntries({ content_type: 'migrationVersion' }).then(entries => {
+    if(entries.total !== 1) {
+        console.error ('More than one migration version record');
+    }
+    var item = entries.items[0];
+    console.log(item.fields.version);
+});
