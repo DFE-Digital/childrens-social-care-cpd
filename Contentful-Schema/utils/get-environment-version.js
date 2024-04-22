80483005b7fea8139d91967347c79fd10ee8b9ee
Matt Barton <matt.barton@hippodigital.co.uk>
2024-04-22 11:05:36 +0100

    improve error catching/handling for unprepared environments

--- Contentful-Schema/utils/get-environment-version.js
+++ Contentful-Schema/utils/get-environment-version.js
@@ -9,20 +9,18 @@ var client = contentful.createClient({
     environment: process.env.ENVIRONMENT
 });
 
-try {
-    client.getEntries({ content_type: 'migrationVersion' }).then(entries => {
-        if (entries.total == 0) {
-            console.error (error('migrationVersion content type exists, but no migration version record found. Environment inconsistently prepared.'));
-            process.exit(1);
-        }
-        if (entries.total > 1) {
-            console.error (error('More than one migration version record found.'));
-            process.exit(1);
-        }
-        var item = entries.items[0];
-        console.log(item.fields.version);
-    });
-} catch (x) {
+client.getEntries({ content_type: 'migrationVersion' }).then(entries => {
+    if (entries.total == 0) {
+        console.error (error('migrationVersion content type exists, but no migration version record found. Environment inconsistently prepared.'));
+        process.exit(1);
+    }
+    if (entries.total > 1) {
+        console.error (error('More than one migration version record found.'));
+        process.exit(1);
+    }
+    var item = entries.items[0];
+    console.log(item.fields.version);
+}).catch(x => {
     console.error(error('migrationVersion content type not found - environment not prepared for migrations?'));
     process.exit(1);
-}
+});
