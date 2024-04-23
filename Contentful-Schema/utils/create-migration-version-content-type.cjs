55ca1194d590571941c7e51afb370ed4b6488a14
Matt Barton <matt.barton@hippodigital.co.uk>
2024-04-23 15:59:10 +0100

    added more readable detail to content type

--- Contentful-Schema/utils/create-migration-version-content-type.cjs
+++ Contentful-Schema/utils/create-migration-version-content-type.cjs
@@ -1,7 +1,9 @@
 module.exports = async function (migration) {
     const contentType = migration
         .createContentType('migrationVersion')
-        .name('Migration Version');
+        .name('Migration Version')
+        .description('Should contain a single entry which denotes the schema migration version number most recently applied to the environment.')
+        .displayField('version');
 
     contentType.createField('version')
         .name('Version')
