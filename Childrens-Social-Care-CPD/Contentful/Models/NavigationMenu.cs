e8419c214bbd699849d7870f9b909151b8409ec6
Matt Barton <matt.barton@hippodigital.co.uk>
2024-07-29 11:31:21 +0100

    fix: add option for header onto navigation menu

--- Childrens-Social-Care-CPD/Contentful/Models/NavigationMenu.cs
+++ Childrens-Social-Care-CPD/Contentful/Models/NavigationMenu.cs
@@ -1,4 +1,5 @@
-﻿using Contentful.Core.Models;
+﻿using System.Runtime.CompilerServices;
+using Contentful.Core.Models;
 
 namespace Childrens_Social_Care_CPD.Contentful.Models;
 
@@ -6,4 +7,6 @@ public class NavigationMenu: IContent
 {
     public string Name { get; set; }
     public List<ContentLink> Items { get; set; }
+    public string Header { get; set; }
+    public int HeaderLevel { get; set; }
 }
