8fc2a8f34518b2520b62442333e54b4343cf7531
Maria <maria.crawford@education.gov.uk>
2024-08-28 15:12:26 +0100

    rectify error on details view

--- Childrens-Social-Care-CPD/Contentful/Models/Details.cs
+++ Childrens-Social-Care-CPD/Contentful/Models/Details.cs
@@ -5,5 +5,5 @@ namespace Childrens_Social_Care_CPD.Contentful.Models;
 public class Details : IContent
 {
     public string SummaryText { get; set; }
-    public string DetailsText { get; set; }
+    public Document DetailsText { get; set; }
 }
