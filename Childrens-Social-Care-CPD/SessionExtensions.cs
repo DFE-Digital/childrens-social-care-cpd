6cfb22022f7defc5f126e479381c23238304e743
Matt Barton <matt.barton@hippodigital.co.uk>
2024-09-30 09:56:29 +0100

    created extension helpers for setting and getting objects on user session

---
+++ Childrens-Social-Care-CPD/SessionExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json;
+
+#nullable enable
+
+namespace Childrens_Social_Care_CPD;
+
+public static class SessionExtensions
+{
+    public static void Set<T>(this ISession session, string key, T value)
+    {
+        session.SetString(key, JsonConvert.SerializeObject(value));
+    }
+
+    public static T? Get<T>(this ISession session, string key)
+    {
+        var value = session.GetString(key);
+        return value == null ? default : JsonConvert.DeserializeObject<T>(value);
+    }
+}
