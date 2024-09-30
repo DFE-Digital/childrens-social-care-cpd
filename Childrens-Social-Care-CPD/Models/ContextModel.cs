ac5dc350e1b050c208100917ce5067bbbbcc423e
Matt Barton <matt.barton@hippodigital.co.uk>
2024-09-30 09:58:44 +0100

    add placeholder for page's breadcrumb trail to context model

--- Childrens-Social-Care-CPD/Models/ContextModel.cs
+++ Childrens-Social-Care-CPD/Models/ContextModel.cs
@@ -2,7 +2,17 @@
 
 namespace Childrens_Social_Care_CPD.Models;
 
-public record ContextModel(string Id, string Title, string PageName, string Category, bool UseContainers, bool PreferenceSet, bool HideConsent = false, ContentLink BackLink = null, bool FeedbackSubmitted = false)
+public record ContextModel(
+    string Id,
+    string Title,
+    string PageName,
+    string Category,
+    bool UseContainers,
+    bool PreferenceSet,
+    bool HideConsent = false,
+    ContentLink BackLink = null,
+    bool FeedbackSubmitted = false,
+    List<KeyValuePair<string, string>> BreadcrumbTrail = null)
 {
     public Stack<string> ContentStack { get; } = new Stack<string>();
 }
