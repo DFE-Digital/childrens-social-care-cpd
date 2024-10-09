689941438725dce5f5a73df5966ed387472bb4c5
Maria <maria.crawford@education.gov.uk>
2024-10-09 10:45:52 +0100

    numbered list rendering correctly

--- Childrens-Social-Care-CPD/Contentful/Renderers/ListRenderer.cs
+++ Childrens-Social-Care-CPD/Contentful/Renderers/ListRenderer.cs
@@ -1,21 +1,33 @@
-using Contentful.Core.Models;
+﻿using Contentful.Core.Models;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Text = Contentful.Core.Models.Text;
 
 namespace Childrens_Social_Care_CPD.Contentful.Renderers;
 
-public class OrderedListRenderer(IRenderer<Text> textRenderer, IRenderer<Hyperlink> hyperlinkRenderer) : IRenderer<List>
+public class ListRenderer(IRenderer<Text> textRenderer, IRenderer<Hyperlink> hyperlinkRenderer) : IRenderer<List>
 {
-    public IHtmlAsyncContent Render(List item)
+    public IHtmlContent Render(List item)
     {
         if (item.Content.Count == 0)
         {
             return null;
         }
 
-        var ol = new TagBuilder("ol");
-        ol.AddCssClass("govuk-list govuk-list--number");
+        // if node type = ol then use tag builder for orderedlist
+        TagBuilder listTag;
+        string cssClass;
+        if (item.NodeType == "unordered-list")
+        {
+            listTag = new TagBuilder("ul");
+            cssClass = "govuk-list govuk-list--bullet";
+        }
+        else
+        {
+            listTag = new TagBuilder("ol");
+            cssClass = "govuk-list govuk-list--number";
+        }
+        listTag.AddCssClass(cssClass);
 
         foreach (var listItem in item.Content.OfType<ListItem>())
         {
@@ -38,13 +50,9 @@ public class OrderedListRenderer(IRenderer<Text> textRenderer, IRenderer<Hyperli
                         }
                 }
             }
-            ol.InnerHtml.AppendHtml(li);
+            listTag.InnerHtml.AppendHtml(li);
         }
-        return (IHtmlAsyncContent)ol;
-    }
 
-    IHtmlContent IRenderer<List>.Render(List item)
-    {
-        throw new NotImplementedException();
+        return listTag;
     }
 }
