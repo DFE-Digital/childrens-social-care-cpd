42b27d8589afd19af76ec17eed45b7aff4081cfe
Matt Barton <matt.barton@hippodigital.co.uk>
2024-12-12 14:37:07 +0000

    feat: pathways contents page rendering

--- Childrens-Social-Care-CPD-Tests/Contentful/Navigation/PathwaysNavigationHelperTests.cs
+++ Childrens-Social-Care-CPD-Tests/Contentful/Navigation/PathwaysNavigationHelperTests.cs
@@ -12,6 +12,8 @@ namespace Childrens_Social_Care_CPD_Tests.Contentful.Navigation;
 
 public class PathwaysNavigationHelperTests
 {
+
+    #region Overview Page
     [Test]
     public async Task Page_Of_Type_Pathways_Overview_Page_Not_Associated_With_Pathways_Module_Should_Render_With_Default_Next () {
 
@@ -68,6 +70,10 @@ public class PathwaysNavigationHelperTests
         // assert
         sut.Next.Url.Should().Be("/" + contentsPageId);
     }
+
+    #endregion
+
+    #region Contents Page
 
     [Test]
     public async Task Page_Of_Type_Pathways_Contents_Page_Should_Have_First_Page_Of_First_Section_Id_As_Next_Url () {
@@ -157,4 +163,283 @@ public class PathwaysNavigationHelperTests
         // assert
         sut.Next.Url.Should().Be("/");
     }
+
+    #endregion
+
+    #region Training Content Pages
+
+    [Test]
+    public async Task First_Page_In_First_Section_Of_Module_Should_Have_Second_Page_Of_First_Section_As_Next_Url_And_Module_Contents_Page_As_Previous_Url () {
+
+        // setup
+        var page = new Content ()
+        {
+            PageType = PageType.PathwaysTrainingContent,
+            Id = "section 1 page 1",
+            PathwaysModule = new PathwaysModule()
+            {
+                ContentsPage = new Content {
+                    Id = "contents page"
+                },
+                Sections = new List<PathwaysModuleSection>()
+                {
+                    new PathwaysModuleSection
+                    {
+                        Pages = new List<Content>() 
+                        {
+                            new Content()
+                            {
+                                Id = "section 1 page 1"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 2"
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        // act
+        var sut = new PathwaysNavigationHelper(page);
+
+        // assert
+        sut.Next.Name.Should().Be("Next");
+        sut.Next.Url.Should().Be("/section 1 page 2");
+        sut.Previous.Name.Should().Be("Previous");
+        sut.Previous.Url.Should().Be("/contents page");
+    }
+
+    [Test]
+    public async Task Middle_Page_In_First_Section_Of_Module_Should_Have_Next_Page_Of_Same_Section_As_Next_Url_And_Previous_Page_Of_Same_Section_As_Previous_Url () {
+
+        // setup
+        var page = new Content ()
+        {
+            PageType = PageType.PathwaysTrainingContent,
+            Id = "section 1 page 2",
+            PathwaysModule = new PathwaysModule()
+            {
+                Sections = new List<PathwaysModuleSection>()
+                {
+                    new PathwaysModuleSection
+                    {
+                        Pages = new List<Content>() 
+                        {
+                            new Content()
+                            {
+                                Id = "section 1 page 1"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 2"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 3"
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        // act
+        var sut = new PathwaysNavigationHelper(page);
+
+        // assert
+        sut.Next.Name.Should().Be("Next");
+        sut.Next.Url.Should().Be("/section 1 page 3");
+        sut.Previous.Name.Should().Be("Previous");
+        sut.Previous.Url.Should().Be("/section 1 page 1");
+    }
+
+    [Test]
+    public async Task Last_Page_In_Not_Last_Section_Of_Module_Should_Have_First_Page_Of_Next_Section_As_Next_Url_And_Previous_Page_Of_Same_Section_As_Previous_Url () {
+
+        // setup
+        var page = new Content ()
+        {
+            PageType = PageType.PathwaysTrainingContent,
+            Id = "section 1 page 3",
+            PathwaysModule = new PathwaysModule()
+            {
+                Sections = new List<PathwaysModuleSection>()
+                {
+                    new PathwaysModuleSection
+                    {
+                        Pages = new List<Content>() 
+                        {
+                            new Content()
+                            {
+                                Id = "section 1 page 1"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 2"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 3"
+                            }
+                        }
+                    },
+                    new PathwaysModuleSection
+                    {
+                        Pages = new List<Content>() 
+                        {
+                            new Content()
+                            {
+                                Id = "section 2 page 1"
+                            },
+                            new Content()
+                            {
+                                Id = "section 2 page 2"
+                            },
+                            new Content()
+                            {
+                                Id = "section 2 page 3"
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        // act
+        var sut = new PathwaysNavigationHelper(page);
+
+        // assert
+        sut.Next.Name.Should().Be("Next");
+        sut.Next.Url.Should().Be("/section 2 page 1");
+        sut.Previous.Name.Should().Be("Previous");
+        sut.Previous.Url.Should().Be("/section 1 page 2");
+    }
+
+    [Test]
+    public async Task First_Page_In_Not_First_Section_Of_Module_Should_Have_Next_Page_Of_Same_Section_As_Next_Url_And_Last_Page_Of_Previous_Section_As_Previous_Url () {
+
+        // setup
+        var page = new Content ()
+        {
+            PageType = PageType.PathwaysTrainingContent,
+            Id = "section 2 page 1",
+            PathwaysModule = new PathwaysModule()
+            {
+                Sections = new List<PathwaysModuleSection>()
+                {
+                    new PathwaysModuleSection
+                    {
+                        Pages = new List<Content>() 
+                        {
+                            new Content()
+                            {
+                                Id = "section 1 page 1"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 2"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 3"
+                            }
+                        }
+                    },
+                    new PathwaysModuleSection
+                    {
+                        Pages = new List<Content>() 
+                        {
+                            new Content()
+                            {
+                                Id = "section 2 page 1"
+                            },
+                            new Content()
+                            {
+                                Id = "section 2 page 2"
+                            },
+                            new Content()
+                            {
+                                Id = "section 2 page 3"
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        // act
+        var sut = new PathwaysNavigationHelper(page);
+
+        // assert
+        sut.Next.Name.Should().Be("Next");
+        sut.Next.Url.Should().Be("/section 2 page 2");
+        sut.Previous.Name.Should().Be("Previous");
+        sut.Previous.Url.Should().Be("/section 1 page 3");
+    }
+
+    [Test]
+    public async Task Last_Page_In_Last_Section_Of_Module_Should_Have_All_Pathways_Page_Next_Url_And_Previous_Page_Of_Same_Section_As_Previous_Url () {
+
+        // setup
+        var page = new Content ()
+        {
+            PageType = PageType.PathwaysTrainingContent,
+            Id = "section 2 page 3",
+            PathwaysModule = new PathwaysModule()
+            {
+                Sections = new List<PathwaysModuleSection>()
+                {
+                    new PathwaysModuleSection
+                    {
+                        Pages = new List<Content>() 
+                        {
+                            new Content()
+                            {
+                                Id = "section 1 page 1"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 2"
+                            },
+                            new Content()
+                            {
+                                Id = "section 1 page 3"
+                            }
+                        }
+                    },
+                    new PathwaysModuleSection
+                    {
+                        Pages = new List<Content>() 
+                        {
+                            new Content()
+                            {
+                                Id = "section 2 page 1"
+                            },
+                            new Content()
+                            {
+                                Id = "section 2 page 2"
+                            },
+                            new Content()
+                            {
+                                Id = "section 2 page 3"
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        // act
+        var sut = new PathwaysNavigationHelper(page);
+
+        // assert
+        sut.Next.Name.Should().Be("Go back to all pathways");
+        sut.Next.Url.Should().Be("/all-pathways");
+        sut.Previous.Name.Should().Be("Previous");
+        sut.Previous.Url.Should().Be("/section 2 page 2");
+    }
+
+    #endregion
 }
--- Childrens-Social-Care-CPD/Contentful/Navigation/PathwaysNavigationHelper.cs
+++ Childrens-Social-Care-CPD/Contentful/Navigation/PathwaysNavigationHelper.cs
@@ -1,16 +1,14 @@
 using Azure;
 using Childrens_Social_Care_CPD.Contentful.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Childrens_Social_Care_CPD.Contentful.Navigation;
 
 public class PathwaysNavigationHelper : INavigationHelper
 {
     // fields
-    private readonly NavigationLocation _next;
-    /*
-    * restore this when implementing main training content
+    private NavigationLocation _next;
     private NavigationLocation _previous;
-    */
 
     // properties
     public NavigationLocation Next
@@ -20,8 +18,6 @@ public class PathwaysNavigationHelper : INavigationHelper
             return _next;
         }
     }
-    /*
-    * restore this when implementing main training content
     public NavigationLocation Previous
     {
         get
@@ -29,7 +25,6 @@ public class PathwaysNavigationHelper : INavigationHelper
             return _previous;
         }
     }
-    */
 
     public PathwaysNavigationHelper (Content page)
     {
@@ -55,12 +50,82 @@ public class PathwaysNavigationHelper : INavigationHelper
             break;
 
             case PageType.PathwaysTrainingContent:
-                /*
-                next page depends on position of current page in sections
-                all but final page just need the next page in the sequence
+                int sectionCounter = 0;
+                bool stop = false;
 
-                previous page depends on position of current page in sections
-                */
+                page.PathwaysModule.Sections.ForEach(section => {
+                    if (stop) return;
+
+                    var pageCounter = 0;
+
+                    section.Pages.ForEach(sectionPage => {
+                        if (stop) return;
+
+                        if (sectionPage.Id == page.Id) {
+                            stop = true;
+                            if (pageCounter < section.Pages.Count()-1) {
+                                // not the last page in the section, make the next link navigate to the next page in the section
+                                this._next = new NavigationLocation {
+                                    Name = "Next",
+                                    Url = "/" + section.Pages[pageCounter + 1].Id
+                                };
+                            }
+                            else 
+                            {
+                                if (sectionCounter < page.PathwaysModule.Sections.Count()-1) {
+                                    // last page in section, but not last section in the module, next navigates to first page in next section
+                                    this._next = new NavigationLocation {
+                                        Name = "Next",
+                                        Url = "/" + page.PathwaysModule.Sections[sectionCounter + 1].Pages[0].Id
+                                    };
+                                }
+                                else{
+                                    // last page in last module, next navigates to 'all pathways page'
+                                    this._next = new NavigationLocation {
+                                        Url = "/all-pathways",
+                                        Name = "Go back to all pathways"
+                                    };
+                                }
+                            }
+
+                            if (pageCounter > 0) {
+                                // not the first page in the section, so previous navigates to previous page
+                                this._previous = new NavigationLocation
+                                {
+                                    Name = "Previous",
+                                    Url = "/" + section.Pages[pageCounter - 1].Id
+                                };
+                            }
+                            else 
+                            {
+                                if (sectionCounter > 0) {
+                                    // first page in section, but not first section, previous should navigate back to last page in previous section
+                                    this._previous = new NavigationLocation
+                                    {
+                                        Name = "Previous",
+                                        Url = "/" + page
+                                            .PathwaysModule
+                                            .Sections[sectionCounter - 1]
+                                            .Pages[page.PathwaysModule.Sections[sectionCounter - 1].Pages.Count()-1]
+                                            .Id
+                                    };
+                                }
+                                else
+                                {
+                                    // first page in first section, previous should navigate back to contents page
+                                    this._previous = new NavigationLocation
+                                    {
+                                        Name = "Previous",
+                                        Url = "/" + page.PathwaysModule.ContentsPage.Id
+                                    };
+                                }
+                            }
+                        }
+                        pageCounter++;
+                    });
+                    sectionCounter++;
+                });
+
             break;
         }
     }
