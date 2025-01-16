dc6d0980b52a1401d5fea5910a0a8d9b39252339
Matt Barton <matt.barton@hippodigital.co.uk>
2025-01-16 12:10:19 +0000

    create separate migration for updating pathways module section name field help text

--- Contentful-Schema/migrations/0020-add-new-display-text-to-pathways-module-section.cjs
+++ Contentful-Schema/migrations/0020-add-new-display-text-to-pathways-module-section.cjs
@@ -19,12 +19,4 @@ module.exports = async function (migration) {
       helpText:
         "This will appear in the Table of Contents so does not need to include the Pathway number. E.g. Introduction to this pathway, Providing clarity, etc. If left blank, the Table of Contents will default to the full Module Section Name.",
     });
-/*
-  migration
-    .editContentType(contentTypeId)
-    .changeFieldControl("name", "builtin", "singleLine", {
-      helpText:
-        "This will appear in the header bar and will also be useful for identifying the section in Contentful. E.g. Pathway 1: Introduction to this pathway, Pathway 1: Providing clarity, etc.",
-    });
-*/
 };
