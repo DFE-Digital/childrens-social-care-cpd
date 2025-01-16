9631c1e0c9a77ab7db1ba020daa41e4b8f21776e
Matt Barton <matt.barton@hippodigital.co.uk>
2025-01-16 11:13:31 +0000

    change help text for name and shortName fields

--- Contentful-Schema/migrations/0020-add-new-display-text-to-pathways-module-section.cjs
+++ Contentful-Schema/migrations/0020-add-new-display-text-to-pathways-module-section.cjs
@@ -5,7 +5,7 @@ module.exports = async function (migration) {
   migration
     .editContentType(contentTypeId)
     .createField("shortName")
-    .name("Module Section Table of Contents Name")
+    .name("Module Section Short Name")
     .type("Symbol")
     .localized(false)
     .required(false)
@@ -15,13 +15,15 @@ module.exports = async function (migration) {
 
   migration
     .editContentType(contentTypeId)
-    .changeFieldControl(
-      "shortName",
-      "builtin",
-      "singleLine",
-      {
-        helpText:
-          "How the module section will be named on the Table of Contents. Optional (if left blank will default to the Module Section Name)",
-      }
-    );
+    .changeFieldControl("shortName", "builtin", "singleLine", {
+      helpText:
+        "This will appear in the Table of Contents so does not need to include the Pathway number. E.g. Introduction to this pathway, Providing clarity, etc. If left blank, the Table of Contents will default to the full Module Section Name.",
+    });
+
+  migration
+    .editContentType(contentTypeId)
+    .changeFieldControl("name", "builtin", "singleLine", {
+      helpText:
+        "This will appear in the header bar and will also be useful for identifying the section in Contentful. E.g. Pathway 1: Introduction to this pathway, Pathway 1: Providing clarity, etc.",
+    });
 };
