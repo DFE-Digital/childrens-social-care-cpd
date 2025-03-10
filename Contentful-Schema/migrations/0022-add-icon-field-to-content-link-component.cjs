749518720764f65ef8421a99302ab4d759984402
Maria <maria.crawford@education.gov.uk>
2025-03-10 15:50:48 +0000

    WiP: update files to include icon

---
+++ Contentful-Schema/migrations/0022-add-icon-field-to-content-link-component.cjs
@@ -0,0 +1,27 @@
+module.exports = async (migration) => {
+
+  const contentTypeId = "contentLink";
+
+  migration
+    .editContentType(contentTypeId)
+    .createField("icon")
+    .name("Icon")
+    .type("Symbol")
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        in: ['Signpost icon', 'Compass icon']
+      }
+    ])
+    .disabled(false)
+    .omitted(false);
+    
+  migration
+    .editContentType(contentTypeId)
+    .changeFieldControl("icon", "builtin", "radio", {
+      helpText:
+          'You can select an icon to appear before the text of the content link. If left blank, no icon will render.',
+      });
+    
+};
