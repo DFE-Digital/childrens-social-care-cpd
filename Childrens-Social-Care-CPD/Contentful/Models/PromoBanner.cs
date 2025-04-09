7ca7a2e7a4141c7677ea028606baca71d304c926
Maria <maria.crawford@education.gov.uk>
2025-04-09 17:28:16 +0100

    WiP: models and views edited to include promo banner

---
+++ Childrens-Social-Care-CPD/Contentful/Models/PromoBanner.cs
@@ -0,0 +1,10 @@
+﻿using Contentful.Core.Models;
+
+namespace Childrens_Social_Care_CPD.Contentful.Models;
+
+public class PromoBanner : IContent
+{
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public ContentLink TitleLink { get; set; }
+}
