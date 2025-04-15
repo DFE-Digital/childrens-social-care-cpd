74f5a2f3d69bb4d1e33c0e369a08ad9cad1131b0
Maria <maria.crawford@education.gov.uk>
2025-04-15 16:03:07 +0100

    WiP: PromoBanner controller amended with ColumnLayout

--- Childrens-Social-Care-CPD/Contentful/Models/PromoBanner.cs
+++ Childrens-Social-Care-CPD/Contentful/Models/PromoBanner.cs
@@ -6,5 +6,5 @@ public class PromoBanner : IContent
 {
     public string Title { get; set; }
     public string Text { get; set; }
-    public ContentLink TitleLink { get; set; }
+    public ColumnLayout ColumnLayout { get; set; }
 }
